08581a2ba9e593d1bb4cb7186b1c9d77acf0023b
Timofey Kirillov <timofey.kirillov@flant.com>
2023-07-04 19:03:24 +0300

    fix(vmd): added protection finalizer for a DV created by a VMD (same as for PV & PVC) (#25)

--- api/v2alpha1/finalizers.go
+++ api/v2alpha1/finalizers.go
@@ -1,6 +1,7 @@
 package v2alpha1
 
 const (
+	FinalizerDVProtection  = "virtualization.deckhouse.io/dv-protection"
 	FinalizerPVCProtection = "virtualization.deckhouse.io/pvc-protection"
 	FinalizerPVProtection  = "virtualization.deckhouse.io/pv-protection"
 	FinalizerVMDCleanup    = "virtualization.deckhouse.io/vmd-cleanup"
