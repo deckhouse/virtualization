4d9e6c0faaa6363fd7014fffefec583b978da23e
Timofey Kirillov <timofey.kirillov@flant.com>
2023-07-04 15:34:19 +0300

    feat(vmd): added finalizers for PVC&PV & cleanup procedure for VMD deletion case (#24)
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

---
+++ api/v2alpha1/finalizers.go
@@ -0,0 +1,7 @@
+package v2alpha1
+
+const (
+	FinalizerPVCProtection = "virtualization.deckhouse.io/pvc-protection"
+	FinalizerPVProtection  = "virtualization.deckhouse.io/pv-protection"
+	FinalizerVMDCleanup    = "virtualization.deckhouse.io/vmd-cleanup"
+)
