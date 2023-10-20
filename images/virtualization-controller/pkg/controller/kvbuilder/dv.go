5091b259e5e771e01437b1b8c87d7a62db706bfe
Ivan Mikheykin <ivan.mikheykin@flant.com>
2023-10-20 15:33:46 +0300

    fix(vmi,vmd): use accessMode ReadWriteMany for created PVC by default

--- images/virtualization-controller/pkg/controller/kvbuilder/dv.go
+++ images/virtualization-controller/pkg/controller/kvbuilder/dv.go
@@ -1,12 +1,12 @@
 package kvbuilder
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	cdiv1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
 
+	"github.com/deckhouse/virtualization-controller/pkg/common/pvc"
 	"github.com/deckhouse/virtualization-controller/pkg/sdk/framework/helper"
 )
 
@@ -37,15 +37,7 @@ func NewDV(name types.NamespacedName) *DV {
 }
 
 func (b *DV) SetPVC(storageClassName string, size resource.Quantity) {
-	b.Resource.Spec.PVC = &corev1.PersistentVolumeClaimSpec{
-		StorageClassName: &storageClassName,
-		AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce}, // TODO: ensure this mode is appropriate
-		Resources: corev1.ResourceRequirements{
-			Requests: corev1.ResourceList{
-				corev1.ResourceStorage: size,
-			},
-		},
-	}
+	b.Resource.Spec.PVC = pvc.CreateSpecForDataVolume(storageClassName, size)
 }
 
 func (b *DV) SetRegistryDataSource(imageName string) {
