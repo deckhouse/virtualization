275737c5c84b4b67a848080c81c4a427ccf0cfe8
Yaroslav Borbat <yaroslav.borbat@flant.com>
2024-03-21 19:46:26 +0300

    add
    Signed-off-by: Yaroslav Borbat <yaroslav.borbat@flant.com>

---
+++ api/client/examples/list-resources/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/deckhouse/virtualization/api/client/kubecli"
+	"github.com/spf13/pflag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"log"
+	"os"
+	"text/tabwriter"
+)
+
+func main() {
+	// kubecli.DefaultClientConfig() prepares config using kubeconfig.
+	// typically, you need to set env variable, KUBECONFIG=<path-to-kubeconfig>/.kubeconfig
+	clientConfig := kubecli.DefaultClientConfig(&pflag.FlagSet{})
+
+	// retrive default namespace.
+	namespace, _, err := clientConfig.Namespace()
+	if err != nil {
+		log.Fatalf("error in namespace : %v\n", err)
+	}
+
+	// get the virtualization client
+	client, err := kubecli.GetClientFromClientConfig(clientConfig)
+	if err != nil {
+		log.Fatalf("Cannot obtain Virtualization client: %v\n", err)
+	}
+
+	// In all namespaces - namespace should empty
+	// Fetch list of VirtualMachines.
+	vmList, err := client.VirtualMachines(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Cannot fetch VirtualMachines in namespace %s: %v", namespace, err)
+	}
+	// Fetch list of Disks.
+	diskList, err := client.VirtualMachineDisks(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Cannot fetch Disks in namespace %s: %v", namespace, err)
+	}
+	// Fetch list of Images.
+	imgList, err := client.VirtualMachineImages(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Cannot fetch Images in namespace %s: %v", namespace, err)
+	}
+	// Fetch list of ClusterImages.
+	cimgList, err := client.ClusterVirtualMachineImages().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Cannot fetch ClusterImages: %v", err)
+	}
+	// Fetch list of IPAddressClaims.
+	ipcList, err := client.VirtualMachineIPAddressClaims(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Cannot fetch IPAddressClaims in namespace %s: %v", namespace, err)
+	}
+	// Fetch list of VirtualMachineOperations.
+	opsList, err := client.VirtualMachineOperations(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Cannot fetch VirtualMachineOperations in namespace %s: %v", namespace, err)
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', 0)
+	fmt.Fprintln(w, "Type\tName\tNamespace\tStatus")
+
+	for _, obj := range vmList.Items {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%v\n", obj.Kind, obj.Name, obj.Namespace, obj.Status.Phase)
+	}
+	for _, obj := range diskList.Items {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%v\n", obj.Kind, obj.Name, obj.Namespace, obj.Status.Phase)
+	}
+	for _, obj := range imgList.Items {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%v\n", obj.Kind, obj.Name, obj.Namespace, obj.Status.Phase)
+	}
+	for _, obj := range cimgList.Items {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%v\n", obj.Kind, obj.Name, "-", obj.Status.Phase)
+	}
+	for _, obj := range ipcList.Items {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%v\n", obj.Kind, obj.Name, obj.Namespace, obj.Status.Phase)
+	}
+	for _, obj := range opsList.Items {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%v\n", obj.Kind, obj.Name, obj.Namespace, obj.Status.Phase)
+	}
+	w.Flush()
+}
