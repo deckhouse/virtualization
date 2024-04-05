c56910271afbd0264e0f93859d9d42cd5ccabea0
Nikita Korolev <nikita.korolev@flant.com>
2024-04-05 17:24:47 +0300

    chore: resolve rebase conflicts
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- tests/performance/status-access-vms/ansible/run.sh
+++ tests/performance/status-access-vms/ansible/run.sh
@@ -1,45 +1,77 @@
 #!/bin/bash
-SSKKEY="../sshkeys/id_ed"
-NAMESPACE=test-perf
+SSK_KEY="../../ssh/id_ed"
+
+function Help() {
+# Display Help
+   cat <<EOF
+
+Syntax: scriptTemplate [-s|n|h]:
+options:
+n     Set namespace with VirtualMachines
+s     Path to ssh private key, default ../../ssh/id_ed
+h     Print this help
+   
+EOF
+}
+
+while getopts "s:n:" opt; do
+  case $opt in
+    s) SSK_KEY=$OPTARG ;;
+    n) NAMESPACE=$OPTARG ;;
+    h) Help
+       exit 1;;
+    \?) echo "Error: Invalid option -$OPTARG" >&2
+        Help
+        exit 1 ;;
+  esac
+done
+
+if [ -z $NAMESPACE ]; then echo "Namespace must be defined"; exit 1;fi
 
 function prepare_ssh_key {
-    chmod 600 $SSKKEY
-    sed -i '' -E "s|private_key_file=.+|private_key_file=${SSKKEY}|" ./ansible.cfg
+    chmod 600 $SSK_KEY
+    sed -i '' -E "s|private_key_file=.+|private_key_file=${SSK_KEY}|" ./ansible.cfg
 }
 
 sigint_handler() {
-    echo "Aborted"
+    echo "Canceld"
     exit 0
 }
 trap 'sigint_handler' SIGINT
 
 
 function generate_inventory {
-    vms=$(kubectl -n $NAMESPACE get vm -o=jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.ipAddress}{"\n"}{end}')
+    VMS=$(kubectl -n $NAMESPACE get vm -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
     mkdir -p inventory
-    inventory_file="inventory/hosts.yml"
+    INVENTORY_FILE="inventory/hosts.yml"
     echo "---
 all:
-  hosts:" > $inventory_file
+  hosts:" > $INVENTORY_FILE
 
-    while IFS=$'\t' read -r vm_name vm_ip; do
-        echo "    ${vm_name}.${NAMESPACE}:" >> $inventory_file
-    done <<< "$vms"
+    while IFS= read -r VM_NAME; do
+        echo "    ${VM_NAME}.${NAMESPACE}:" >> $INVENTORY_FILE
+    done <<< "$VMS"
 }
 
 function main {
     prepare_ssh_key
+    ANSIBLE_REPORT_FILE=play_report.log
 
     while true
     do
         echo "Generate inventory"
         generate_inventory
+        
         echo "Try to access all hosts from inventory "
-        ansible-playbook playbook.yaml | sed -n '/PLAY RECAP/,$p' > play_report.log
-        ALLHOSTS=$(( $(wc -l play_report.log | grep -Eo '\d{1,4}') - 2 )) # One head line and 1 empty at EOF
-        OKHOST=$(( $(grep 'ok=1' play_report.log | wc -l) ))
-        UNREACHABLE=$(( $ALLHOSTS - $OKHOST ))
-        echo "OK hosts $OKHOST | Unreachable hosts $UNREACHABLE | Total hosts $ALLHOSTS"
+        ansible-playbook playbook.yaml | sed -n '/PLAY RECAP/,$p' > $ANSIBLE_REPORT_FILE
+        while [ ! -f $ANSIBLE_REPORT_FILE ]; do sleep 1; done
+        
+        ALL_HOSTS=$(( $(wc -l $ANSIBLE_REPORT_FILE | grep -Eo '\d{1,4}') - 2 )) # One head line and 1 empty at EOF
+        OK_HOSTS=$(( $(grep 'ok=1' $ANSIBLE_REPORT_FILE | wc -l) ))
+        UNREACHABLE_HOSTS=$(( $(grep 'unreachable=1' $ANSIBLE_REPORT_FILE | wc -l) ))
+        OK_PCT=$(bc -l <<< "scale=2; $OK_HOSTS/$ALL_HOSTS*100")
+        
+        echo "OK hosts count:$OK_HOSTS pct.:$OK_PCT% | Unreachable hosts $UNREACHABLE_HOSTS | Total hosts $ALL_HOSTS"
         echo "Wait 1 sec"
         sleep 1
     done
