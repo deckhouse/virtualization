7f79edc663cdccec0ee00c7ccd58d85e19e66ae2
Ivan Mikheykin <ivan.mikheykin@flant.com>
2024-04-25 20:15:55 +0300

    fix(pre-delete-hook): hook should not fail
    - Failed hook leads to a deadlock in module lifecycle
    
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- images/pre-delete-hook/entrypoint.sh
+++ images/pre-delete-hook/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -eu -o pipefail
 
-kubectl delete -n d8-virtualization kubevirts.x.virtualization.deckhouse.io kubevirt
-kubectl delete cdis.x.virtualization.deckhouse.io cdi
-kubectl wait --for=delete cdis.x.virtualization.deckhouse.io cdi --timeout=180s
-kubectl wait --for=delete -n d8-virtualization kubevirts.x.virtualization.deckhouse.io kubevirt --timeout=180s
+KUBEVIRT_RESOURCE="kubevirts.x.virtualization.deckhouse.io"
+echo "Delete Kubevirt configuration ..."
+kubectl delete -n d8-virtualization ${KUBEVIRT_RESOURCE} kubevirt || true
+echo "Wait for Kubevirt deletion ..."
+kubectl wait --for=delete -n d8-virtualization ${KUBEVIRT_RESOURCE} kubevirt --timeout=180s || true
+
+CDI_RESOURCE="cdis.x.virtualization.deckhouse.io"
+echo "Delete CDI configuration ..."
+kubectl delete ${CDI_RESOURCE} cdi || true
+echo "Wait for CDI deletion ..."
+kubectl wait --for=delete ${CDI_RESOURCE} cdi --timeout=180s || true
