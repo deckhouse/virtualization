711dacae04a8069b2314d3cca783d7509efc33a8
Ivan Mikheykin <ivan.mikheykin@flant.com>
2024-04-26 16:08:48 +0300

    refactor(vmi-router): Use IP from our VirtualMachine (#69)
    * refactor(vmi-router): Use IP from our VirtualMachine
    
    - Use status.ipaddress instead of extracting ip from huge status in Kubevirt VM.
    - Add environment variable CILIUM_ROUTE_TABLE_ID for route table id.
    - Refactor: proper use of controller-runtime.
    - Deps: drop kubevirt.io/client, update netlink, cilium, controller-runtime, k8 dependencies.
    - Fix: add aliases for netlink methods to develop with comfort in non-linux OSes.
    - Add golang 21 base image.
    
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- images/vmi-router/controllers/suite_test.go
+++ images/vmi-router/controllers/suite_test.go
@@ -28,7 +28,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	//+kubebuilder:scaffold:imports
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
@@ -48,8 +47,8 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: false,
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "crds")},
+		ErrorIfCRDPathMissing: true,
 	}
 
 	var err error
@@ -58,13 +57,10 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	//+kubebuilder:scaffold:scheme
-
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
-
-}, 60)
+})
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
