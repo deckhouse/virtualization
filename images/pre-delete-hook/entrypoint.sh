ff32d55fb55e2f7b93dbc278659edd1dd10c7c17
Pavel Tishkov <pavel.tishkov@flant.com>
2024-05-20 18:58:38 +0300

    feat(module): rename core resources (#104)
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- images/pre-delete-hook/entrypoint.sh
+++ images/pre-delete-hook/entrypoint.sh
@@ -3,12 +3,12 @@ set -eu -o pipefail
 
 KUBEVIRT_RESOURCE="dvpinternalkubevirts.internal.virtualization.deckhouse.io"
 echo "Delete Kubevirt configuration ..."
-kubectl delete -n d8-virtualization ${KUBEVIRT_RESOURCE} kubevirt || true
+kubectl delete -n d8-virtualization ${KUBEVIRT_RESOURCE} config || true
 echo "Wait for Kubevirt deletion ..."
-kubectl wait --for=delete -n d8-virtualization ${KUBEVIRT_RESOURCE} kubevirt --timeout=180s || true
+kubectl wait --for=delete -n d8-virtualization ${KUBEVIRT_RESOURCE} config --timeout=180s || true
 
 CDI_RESOURCE="dvpinternalcdis.internal.virtualization.deckhouse.io"
 echo "Delete CDI configuration ..."
-kubectl delete ${CDI_RESOURCE} cdi || true
+kubectl delete ${CDI_RESOURCE} config || true
 echo "Wait for CDI deletion ..."
-kubectl wait --for=delete ${CDI_RESOURCE} cdi --timeout=180s || true
+kubectl wait --for=delete ${CDI_RESOURCE} config --timeout=180s || true
