0c0bed21b27cde05f608d827f7e834696c5fdcb9
Nikita Korolev <nikita.korolev@flant.com>
2024-06-12 01:49:02 +0300

    chore: refactor tool add CE license
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- tools/addlic/go.mod
+++
@@ -1,3 +0,0 @@
-module addlic
-
-go 1.21.4
--- tools/addlicense/msg.go
+++ tools/addlicense/msg.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-const OKType = "OK"
-const SkipType = "Skip"
-const ErrorType = "ERROR"
+const TypeAdd = "Add"
+const TypeSkip = "Skip"
+const TypeError = "ERROR"
 
 type Message struct {
 	Type     string
@@ -13,42 +13,16 @@ type Message struct {
 	Details  string
 }
 
-func (msg Message) IsError() bool {
-	return msg.Type == ErrorType
-}
-
-func (msg Message) IsSkip() bool {
-	return msg.Type == SkipType
-}
-
-func (msg Message) IsOK() bool {
-	return msg.Type == OKType
-}
-
-func (msg Message) Format() string {
-	res := ""
-	if msg.Message == "" {
-		res += fmt.Sprintf("  * %s ... %s", msg.FileName, msg.Type)
-	} else {
-		res += fmt.Sprintf("  * %s ... %s: %s", msg.FileName, msg.Type, msg.Message)
-	}
-	if msg.Details != "" {
-		res += fmt.Sprintf("  * %s ... %s: %s", msg.FileName, msg.Type, msg.Message)
-		//res += "\n" + indentTextBlock(msg.Details, 6)
-	}
-	return res
-}
-
-func NewOK(fileName string) Message {
+func NewAdd(fileName string) Message {
 	return Message{
-		Type:     OKType,
+		Type:     TypeAdd,
 		FileName: fileName,
 	}
 }
 
 func NewSkip(fileName string, msg string) Message {
 	return Message{
-		Type:     SkipType,
+		Type:     TypeSkip,
 		FileName: fileName,
 		Message:  msg,
 	}
@@ -56,11 +30,23 @@ func NewSkip(fileName string, msg string) Message {
 
 func NewError(fileName string, msg string, details string) Message {
 	return Message{
-		Type:     ErrorType,
+		Type:     TypeError,
 		FileName: fileName,
 		Message:  msg,
 		Details:  details,
 	}
+}
+
+func (msg Message) IsError() bool {
+	return msg.Type == TypeError
+}
+
+func (msg Message) IsSkip() bool {
+	return msg.Type == TypeSkip
+}
+
+func (msg Message) IsAdd() bool {
+	return msg.Type == TypeAdd
 }
 
 type Messages struct {
@@ -77,10 +63,10 @@ func (m *Messages) Add(msg Message) {
 	m.messages = append(m.messages, msg)
 }
 
-func (m *Messages) CountOK() int {
+func (m *Messages) CountAdd() int {
 	res := 0
 	for _, msg := range m.messages {
-		if msg.IsOK() {
+		if msg.IsAdd() {
 			res++
 		}
 	}
@@ -107,6 +93,19 @@ func (m *Messages) CountErrors() int {
 	return res
 }
 
+func (msg Message) Format() string {
+	res := ""
+	if msg.Message == "" {
+		res += fmt.Sprintf("  * %s ... %s", msg.FileName, msg.Type)
+	} else {
+		res += fmt.Sprintf("  * %s ... %s: %s", msg.FileName, msg.Type, msg.Message)
+	}
+	if msg.Details != "" {
+		res += "\n" + msg.Details
+	}
+	return res
+}
+
 func (m *Messages) PrintReport() {
 	if m.CountSkip() > 0 {
 		fmt.Println("Skipped:")
@@ -116,10 +115,10 @@ func (m *Messages) PrintReport() {
 			}
 		}
 	}
-	if m.CountOK() > 0 {
-		fmt.Println("OK:")
+	if m.CountAdd() > 0 {
+		fmt.Println("Add:")
 		for _, msg := range m.messages {
-			if msg.IsOK() {
+			if msg.IsAdd() {
 				fmt.Println(msg.Format())
 			}
 		}
