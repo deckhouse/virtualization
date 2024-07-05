0cce99238408b670c0aee6ac6874ce83995c0d47
Isteb4k <dmitry.rakitin@flant.com>
2024-07-05 13:10:19 +0200

    fix(vmbda): fix hotplug api call
    * fix(api): fix protection of resources during the deletion process
    
    Patched the virt-controller: it now communicates with virt-api via the virtualization-api.
    Previously, during the hot plugging process, virt-controller communicated with virt-api through the KubeVirt API service. However, since this API service was removed, the hot plugging process stopped working.
    
    The Protection service does not attempt to add a finalizer if the resource is in the process of being deleted (to avoid the error No new finalizers can be added if the object is being deleted).
    
    Signed-off-by: Isteb4k <dmitry.rakitin@flant.com>

--- images/virtualization-artifact/pkg/apiserver/registry/vm/rest/stream.go
+++ images/virtualization-artifact/pkg/apiserver/registry/vm/rest/stream.go
@@ -116,6 +116,10 @@ func streamParams(_ url.Values, opts runtime.Object) error {
 		return nil
 	case *subresources.VirtualMachinePortForward:
 		return nil
+	case *subresources.VirtualMachineAddVolume:
+		return nil
+	case *subresources.VirtualMachineRemoveVolume:
+		return nil
 	default:
 		return fmt.Errorf("unknown object for streaming: %v", opts)
 	}
@@ -124,14 +128,14 @@ func streamParams(_ url.Values, opts runtime.Object) error {
 func newThrottledUpgradeAwareProxyHandler(
 	location *url.URL,
 	transport *http.Transport,
-	wrapTransport, upgradeRequired bool,
+	upgradeRequired bool,
 	responder rest.Responder,
 	sa types.NamespacedName,
 ) http.Handler {
 	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		r.Header.Add(userHeader, fmt.Sprintf("system:serviceaccount:%s:%s", sa.Namespace, sa.Name))
 		r.Header.Add(groupHeader, "system:serviceaccounts")
-		proxyHandler := proxy.NewUpgradeAwareHandler(location, transport, wrapTransport, upgradeRequired, proxy.NewErrorResponder(responder))
+		proxyHandler := proxy.NewUpgradeAwareHandler(location, transport, false, upgradeRequired, proxy.NewErrorResponder(responder))
 		proxyHandler.ServeHTTP(w, r)
 	}
 	return handler
