2b4283ffe2dae760d99579ec137f64cbacdb5765
yaroslavborbat <yaroslav.752@gmail.com>
2024-07-17 18:50:16 +0300

    fix panic
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

--- images/virtualization-artifact/pkg/controller/vd/internal/source/upload.go
+++ images/virtualization-artifact/pkg/controller/vd/internal/source/upload.go
@@ -231,6 +231,12 @@ func (ds UploadDataSource) Sync(ctx context.Context, vd *virtv2.VirtualDisk) (bo
 		vd.Status.DownloadSpeed = ds.statService.GetDownloadSpeed(vd.GetUID(), pod)
 
 		return true, nil
+	case pvc == nil:
+		vd.Status.Phase = virtv2.DiskProvisioning
+		condition.Status = metav1.ConditionFalse
+		condition.Reason = vdcondition.Provisioning
+		condition.Message = "PVC not found: waiting for creation."
+		return true, nil
 	case ds.diskService.IsImportDone(dv, pvc):
 		logger.Info("Import has completed", "dvProgress", dv.Status.Progress, "dvPhase", dv.Status.Phase, "pvcPhase", pvc.Status.Phase)
 
