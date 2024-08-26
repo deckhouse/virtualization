f246e866ac2bbd096286dbd008f4893f82fa8068
yaroslavborbat <yaroslav.752@gmail.com>
2024-08-26 13:37:24 +0300

    fix
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

--- images/vm-route-forge/bpf/route_watcher.c
+++ images/vm-route-forge/bpf/route_watcher.c
@@ -1,5 +1,5 @@
-#include "lib/types.h"
-#include "lib/ip_fib_less.h"
+#include "include/types.h"
+#include "include/ip_fib_less.h"
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
@@ -15,6 +15,8 @@ struct route_event {
 // Force emitting struct event into the ELF.
 const struct route_event *unused __attribute__((unused));
 
+// Define a map with the BPF_MAP_TYPE_QUEUE type.
+// BPF_MAP_TYPE_QUEUE provides FIFO storage.
 struct {
     __uint(type, BPF_MAP_TYPE_QUEUE);
     __uint(key_size, 0);
@@ -31,6 +33,7 @@ static inline int insert_event(struct pt_regs *ctx, u32 action) {
     struct fib_config *cfg;
     int ret;
 
+    // Save the fib_table from the ctx to the tb.
     ret = bpf_probe_read_kernel(&tb, sizeof(tb), (void *)&PT_REGS_PARM2(ctx));
     if (!tb) {
         static const char msg[] = "Failed to read fib_table pointer: %d";
@@ -38,6 +41,7 @@ static inline int insert_event(struct pt_regs *ctx, u32 action) {
         return ret;
     }
 
+    // Save the table id from the tb to the evt.
     ret = bpf_probe_read_kernel(&evt.table, sizeof(evt.table), &tb->tb_id);
     if (ret < 0) {
         static const char msg[] = "Failed to read tb_id: %d";
@@ -45,6 +49,7 @@ static inline int insert_event(struct pt_regs *ctx, u32 action) {
         return ret;
     }
 
+    // Save the fib_config from the ctx to the cfg.
     ret = bpf_probe_read_kernel(&cfg, sizeof(cfg), (void *)&PT_REGS_PARM3(ctx));
     if (!cfg) {
         static const char msg[] = "Failed to read fib_config pointer: %d";
@@ -52,6 +57,7 @@ static inline int insert_event(struct pt_regs *ctx, u32 action) {
         return ret;
     }
 
+    // Save the dst address from the cfg to the evt.
     ret = bpf_probe_read_kernel(&evt.dst, sizeof(evt.dst), &cfg->fc_dst);
     if (ret < 0) {
         static const char msg[] = "Failed to read dst: %d";
@@ -59,6 +65,7 @@ static inline int insert_event(struct pt_regs *ctx, u32 action) {
         return ret;
     }
 
+    // Save the src address from the cfg to the evt.
     ret = bpf_probe_read_kernel(&evt.src, sizeof(evt.src), &cfg->fc_prefsrc);
     if (ret < 0) {
         static const char msg[] = "Failed to read src: %d";
@@ -66,16 +73,24 @@ static inline int insert_event(struct pt_regs *ctx, u32 action) {
         return ret;
     }
 
+    // Add route_event to the end of the bpf map.
+    // We never delete objects from the map from the bpf program, because our program does this in the user namespace.
     return bpf_map_push_elem(&route_events_map, &evt, BPF_ANY);
 }
 
 SEC("kprobe/fib_table_insert")
+// int fib_table_insert(struct net *, struct fib_table *, struct fib_config *,
+//   		     struct netlink_ext_ack *extack);
+// The parameters of fib_table_insert are saved in ctx
 int fib_table_insert(struct pt_regs *ctx) {
     insert_event(ctx, 0);
     return 0;
 }
 
 SEC("kprobe/fib_table_delete")
+// int fib_table_delete(struct net *, struct fib_table *, struct fib_config *,
+//   		     struct netlink_ext_ack *extack);
+// The parameters of fib_table_delete are saved in ctx
 int fib_table_delete(struct pt_regs *ctx) {
     insert_event(ctx, 1);
     return 0;
