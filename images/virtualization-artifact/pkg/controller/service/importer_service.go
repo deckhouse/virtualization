7661e747509f7791ef6afacafb440cfda6763389
Dmitry Lopatin <dmitry.lopatin@flant.com>
2024-09-03 14:23:24 +0300

    fix
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/service/importer_service.go
+++ images/virtualization-artifact/pkg/controller/service/importer_service.go
@@ -76,7 +76,7 @@ func (s ImporterService) Start(ctx context.Context, settings *importer.Settings,
 	return supplements.EnsureForPod(ctx, s.client, sup, pod, caBundle, s.dvcrSettings)
 }
 
-func (s ImporterService) StartFromPVC(ctx context.Context, settings *importer.Settings, obj ObjectKind, sup *supplements.Generator, caBundle *datasource.CABundle, pvcName string, pvcNamespace string) error {
+func (s ImporterService) StartFromPVC(ctx context.Context, settings *importer.Settings, obj ObjectKind, sup *supplements.Generator, caBundle *datasource.CABundle, pvcName, pvcNamespace string) error {
 	ownerRef := metav1.NewControllerRef(obj, obj.GroupVersionKind())
 	settings.Verbose = s.verbose
 
--- images/virtualization-artifact/pkg/controller/vi/internal/source/interfaces.go
+++ images/virtualization-artifact/pkg/controller/vi/internal/source/interfaces.go
@@ -36,7 +36,7 @@ import (
 
 type Importer interface {
 	Start(ctx context.Context, settings *importer.Settings, obj service.ObjectKind, sup *supplements.Generator, caBundle *datasource.CABundle) error
-	StartFromPVC(ctx context.Context, settings *importer.Settings, obj service.ObjectKind, sup *supplements.Generator, caBundle *datasource.CABundle, pvcName string, pvcNamespace string) error
+	StartFromPVC(ctx context.Context, settings *importer.Settings, obj service.ObjectKind, sup *supplements.Generator, caBundle *datasource.CABundle, pvcName, pvcNamespace string) error
 	CleanUp(ctx context.Context, sup *supplements.Generator) (bool, error)
 	CleanUpSupplements(ctx context.Context, sup *supplements.Generator) (bool, error)
 	GetPod(ctx context.Context, sup *supplements.Generator) (*corev1.Pod, error)
