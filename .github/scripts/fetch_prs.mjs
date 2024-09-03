936164701be93a627bee5fa600dc50ca43d87df7
Nikita Korolev <141920865+universal-itengineer@users.noreply.github.com>
2024-09-03 11:51:39 +0300

    chore(ci): fix workflow PRs summary (#313)
    Fix workflow PRs summary
    ---------
    
    Signed-off-by: Nikita korolev <nikita.korolev@flant.com>
    Co-authored-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- .github/scripts/fetch_prs.mjs
+++ .github/scripts/fetch_prs.mjs
@@ -12,72 +12,104 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 import { Octokit } from '@octokit/core';
 import axios from 'axios';
 import moment from 'moment';
 
 const owner = 'deckhouse';
 const repo = 'virtualization';
-const project = 'DVP'
-
+const project = 'DVP';
+const defaultLogin = 'Almighty PM';
 const octokit = new Octokit({ auth: process.env.RELEASE_PLEASE_TOKEN });
-
-
-async function fetchPullRequests() {
-  const pulls = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
-    owner,
-    repo,
-    state: 'all'
-  });
-  return pulls.data.filter(pr => !pr.draft);
-}
-
 const recentDays = 2;
 
-function generateSummary(prs) {
+async function fetchPullRequests() {
+  try {
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+      owner,
+      repo,
+      per_page: 500,
+      state: 'open',
+    });
+    return data.filter(pr => !pr.draft);
+  } catch (error) {
+    console.error('Error fetching pull requests:', error);
+    throw error;
+  }
+}
+
+async function fetchReviewerDetails(login) {
+  try {
+    const { data } = await octokit.request('GET /users/{username}', {
+      username: login,
+    });
+    return data;
+  } catch (error) {
+    console.error(`Error fetching details for reviewer ${login}:`, error);
+    return null;
+  }
+}
+
+async function formatPR(pr) {
+  let reviewersInfo = `NO REVIEWERS! ${defaultLogin}, your care is required here.`;
+  if (pr.requested_reviewers && pr.requested_reviewers.length > 0) {
+    const reviewers = await Promise.all(
+      pr.requested_reviewers.map(async reviewer => {
+        const details = await fetchReviewerDetails(reviewer.login);
+        return details ? `${details.login} (${details.name})` : reviewer.login;
+      })
+    );
+    reviewersInfo = `Reviewers: ${reviewers.join(', ')}`;
+  }
+
+  return `- [${pr.title}](${pr.html_url}) (Created: ${moment(pr.created_at).fromNow()}) - ${reviewersInfo}`;
+}
+
+async function generateSummary(prs) {
   const now = moment();
-  const reviewRequired = prs.filter(pr => pr.state === 'open' && pr.requested_reviewers.length > 0);
+  const reviewRequired = prs.filter(pr => pr.requested_reviewers.length > 0);
   const recent = reviewRequired.filter(pr => moment().diff(moment(pr.created_at), 'days') <= recentDays);
   const lasting = reviewRequired.filter(pr => moment().diff(moment(pr.created_at), 'days') > recentDays);
 
   let summary = `## ${project} PRs ${now.format('YYYY-MM-DD')}\n\n`;
 
-  if (reviewRequired.length == 0) {
+  if (reviewRequired.length === 0) {
     summary += `:tada: No review required for today\n`;
     return summary;
   }
-  
-  if (recent.length > 0) {
-    summary += `### Recent PRs requiring review
 
-${recent.map(pr => `- [${pr.title}](${pr.html_url}) (Created: ${moment(pr.created_at).fromNow()})`).join('\n')}
-`;
+  if (recent.length > 0) {
+    const recentPRsInfo = await Promise.all(recent.map(formatPR));
+    summary += `### Recent PRs requiring review\n\n${recentPRsInfo.join('\n')}\n\n`;
   }
 
   if (lasting.length > 0) {
-    summary += `### PRs requiring review
-
-${lasting.map(pr => `- [${pr.title}](${pr.html_url}) (Created: ${moment(pr.created_at).fromNow()})`).join('\n')}
-`;
+    const lastingPRsInfo = await Promise.all(lasting.map(formatPR));
+    summary += `### PRs requiring review\n\n${lastingPRsInfo.join('\n')}\n`;
   }
 
-  return summary
+  return summary;
 }
 
 async function sendSummaryToLoop(summary) {
   const url = process.env.LOOP_WEBHOOK_URL;
-  await axios.post(url, {
-    text: summary
-  });
+  try {
+    await axios.post(url, { text: summary });
+    console.log('Summary sent successfully.');
+  } catch (error) {
+    console.error('Error sending summary to Loop:', error);
+    throw error;
+  }
 }
 
 async function run() {
   try {
     const prs = await fetchPullRequests();
-    const summary = generateSummary(prs);
+    const summary = await generateSummary(prs);
     await sendSummaryToLoop(summary);
   } catch (error) {
-    console.error(error);
+    console.error('An error occurred:', error);
     process.exit(1);
   }
 }
