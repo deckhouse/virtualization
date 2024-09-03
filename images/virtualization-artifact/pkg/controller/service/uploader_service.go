b6661ba84604b8fc131493d94f3fc63ac66589af
Dmitry Lopatin <dmitry.lopatin@flant.com>
2024-09-03 16:55:07 +0300

    add debug logs
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/service/uploader_service.go
+++ images/virtualization-artifact/pkg/controller/service/uploader_service.go
@@ -74,7 +74,7 @@ func (s UploaderService) Start(ctx context.Context, settings *uploader.Settings,
 		return err
 	}
 
-	err = supplements.EnsureForPod(ctx, s.client, sup, pod, caBundle, s.dvcrSettings, false)
+	err = supplements.EnsureForPod(ctx, s.client, sup, pod, caBundle, s.dvcrSettings)
 	if err != nil {
 		return err
 	}
