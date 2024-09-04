b28a66953854f3616534b54cf0c4a046653bf15f
Dmitry Lopatin <dmitry.lopatin@flant.com>
2024-09-04 19:58:54 +0300

    fix(vi): add fake validate
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/cvi/internal/source/object_ref_vd.go
+++ images/virtualization-artifact/pkg/controller/cvi/internal/source/object_ref_vd.go
@@ -215,3 +215,8 @@ func (ds ObjectRefVirtualDisk) getEnvSettings(cvi *virtv2.ClusterVirtualImage, s
 
 	return &settings
 }
+
+func (ds ObjectRefVirtualDisk) Validate(_ context.Context, _ *virtv2.ClusterVirtualImage) error {
+	// todo
+	return nil
+}
