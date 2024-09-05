d20e2b843234e042da2aafcae2920dfa315f38b5
Dmitry Lopatin <dmitry.lopatin@flant.com>
2024-09-05 10:06:35 +0300

    fix(cvi): fix clean up
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/cvi/internal/source/object_ref_vd.go
+++ images/virtualization-artifact/pkg/controller/cvi/internal/source/object_ref_vd.go
@@ -188,7 +188,7 @@ func (ds ObjectRefVirtualDisk) CleanUpSupplements(ctx context.Context, cvi *virt
 }
 
 func (ds ObjectRefVirtualDisk) CleanUp(ctx context.Context, cvi *virtv2.ClusterVirtualImage) (bool, error) {
-	supgen := supplements.NewGenerator(cc.CVIShortName, cvi.Name, ds.controllerNamespace, cvi.UID)
+	supgen := supplements.NewGenerator(cc.CVIShortName, cvi.Name, cvi.Spec.DataSource.ObjectRef.Namespace, cvi.UID)
 
 	importerRequeue, err := ds.importerService.CleanUp(ctx, supgen)
 	if err != nil {
