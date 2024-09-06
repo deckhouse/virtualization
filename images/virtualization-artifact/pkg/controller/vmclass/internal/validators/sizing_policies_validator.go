5f585c426a03a5875ffcdde807febf35050351b3
fearning <b1kapb999@gmail.com>
2024-09-06 15:42:44 +0300

    feat(vmclass): size policies vaidation hook (#344)
    * feat(vmclass): add vmclass size policies cpu cross validation
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

---
+++ images/virtualization-artifact/pkg/controller/vmclass/internal/validators/sizing_policies_validator.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validators
+
+import (
+	"context"
+	"fmt"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+)
+
+type SizingPoliciesValidator struct {
+	client client.Client
+}
+
+func NewSizingPoliciesValidator(client client.Client) *SizingPoliciesValidator {
+	return &SizingPoliciesValidator{client: client}
+}
+
+func (v *SizingPoliciesValidator) ValidateCreate(_ context.Context, vmclass *v1alpha2.VirtualMachineClass) (admission.Warnings, error) {
+	if HasCpuSizePoliciesCrosses(&vmclass.Spec) {
+		return nil, fmt.Errorf("vmclass %s has size policy cpu crosses", vmclass.Name)
+	}
+
+	return nil, nil
+}
+
+func (v *SizingPoliciesValidator) ValidateUpdate(_ context.Context, _, newVMClass *v1alpha2.VirtualMachineClass) (admission.Warnings, error) {
+	if HasCpuSizePoliciesCrosses(&newVMClass.Spec) {
+		return nil, fmt.Errorf("vmclass %s has size policy cpu crosses", newVMClass.Name)
+	}
+
+	return nil, nil
+}
+
+func HasCpuSizePoliciesCrosses(vmclass *v1alpha2.VirtualMachineClassSpec) bool {
+	usedPairs := make(map[[2]int]struct{})
+
+	for i, policy1 := range vmclass.SizingPolicies {
+		for j, policy2 := range vmclass.SizingPolicies {
+			if i == j {
+				continue
+			}
+			_, ok := usedPairs[[2]int{i, j}]
+			if ok {
+				continue
+			}
+
+			if policy1.Cores.Min >= policy2.Cores.Min && policy1.Cores.Min <= policy2.Cores.Max {
+				return true
+			}
+
+			if policy1.Cores.Max >= policy2.Cores.Min && policy1.Cores.Max <= policy2.Cores.Max {
+				return true
+			}
+
+			usedPairs[[2]int{i, j}] = struct{}{}
+			usedPairs[[2]int{j, i}] = struct{}{}
+		}
+	}
+
+	return false
+}
---
+++ images/virtualization-artifact/pkg/controller/vmclass/internal/validators/validators_suite_test.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validators_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/deckhouse/virtualization-controller/pkg/controller/vmclass/internal/validators"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+)
+
+func TestValidators(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Validators Suite")
+}
+
+var _ = Describe("Spec policies validator", func() {
+	var vmclass v1alpha2.VirtualMachineClass
+
+	Context("empty vmclass", func() {
+		BeforeEach(func() {
+			vmclass = v1alpha2.VirtualMachineClass{}
+		})
+
+		It("Should return no problem when empty value", func() {
+			Expect(validators.HasCpuSizePoliciesCrosses(&vmclass.Spec)).Should(BeFalse())
+		})
+	})
+
+	Context("vmclass with no cpu size policies crosses", func() {
+		BeforeEach(func() {
+			vmclass = v1alpha2.VirtualMachineClass{}
+			vmclass.Spec.SizingPolicies = append(vmclass.Spec.SizingPolicies, v1alpha2.SizingPolicy{
+				Cores: &v1alpha2.SizingPolicyCores{
+					Min:  1,
+					Max:  4,
+					Step: 1,
+				},
+			})
+			vmclass.Spec.SizingPolicies = append(vmclass.Spec.SizingPolicies, v1alpha2.SizingPolicy{
+				Cores: &v1alpha2.SizingPolicyCores{
+					Min:  5,
+					Max:  9,
+					Step: 1,
+				},
+			})
+			vmclass.Spec.SizingPolicies = append(vmclass.Spec.SizingPolicies, v1alpha2.SizingPolicy{
+				Cores: &v1alpha2.SizingPolicyCores{
+					Min:  10,
+					Max:  15,
+					Step: 1,
+				},
+			})
+		})
+
+		It("Should return no problem with correct values", func() {
+			Expect(validators.HasCpuSizePoliciesCrosses(&vmclass.Spec)).Should(BeFalse())
+		})
+	})
+
+	Context("vmclass with cpu size policies crosses", func() {
+		BeforeEach(func() {
+			vmclass = v1alpha2.VirtualMachineClass{}
+			vmclass.Spec.SizingPolicies = append(vmclass.Spec.SizingPolicies, v1alpha2.SizingPolicy{
+				Cores: &v1alpha2.SizingPolicyCores{
+					Min:  1,
+					Max:  4,
+					Step: 1,
+				},
+			})
+			vmclass.Spec.SizingPolicies = append(vmclass.Spec.SizingPolicies, v1alpha2.SizingPolicy{
+				Cores: &v1alpha2.SizingPolicyCores{
+					Min:  4,
+					Max:  9,
+					Step: 1,
+				},
+			})
+			vmclass.Spec.SizingPolicies = append(vmclass.Spec.SizingPolicies, v1alpha2.SizingPolicy{
+				Cores: &v1alpha2.SizingPolicyCores{
+					Min:  10,
+					Max:  15,
+					Step: 1,
+				},
+			})
+		})
+
+		It("Should return problem with incorrect values", func() {
+			Expect(validators.HasCpuSizePoliciesCrosses(&vmclass.Spec)).Should(BeTrue())
+		})
+	})
+})
