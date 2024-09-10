240b21b9a54b34d9eff7594029da82eab40075f9
Dmitry Lopatin <dmitry.lopatin@flant.com>
2024-09-10 12:14:59 +0300

    fix fail condition
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/cvi/internal/datasource_ready.go
+++ images/virtualization-artifact/pkg/controller/cvi/internal/datasource_ready.go
@@ -84,6 +84,11 @@ func (h DatasourceReadyHandler) Handle(ctx context.Context, cvi *virtv2.ClusterV
 		condition.Reason = cvicondition.ClusterImageNotReady
 		condition.Message = service.CapitalizeFirstLetter(err.Error())
 		return reconcile.Result{}, nil
+	case errors.As(err, &source.VirtualDiskNotReadyError{}):
+		condition.Status = metav1.ConditionFalse
+		condition.Reason = cvicondition.VirtualDiskNotReady
+		condition.Message = service.CapitalizeFirstLetter(err.Error())
+		return reconcile.Result{}, nil
 	default:
 		return reconcile.Result{}, err
 	}
