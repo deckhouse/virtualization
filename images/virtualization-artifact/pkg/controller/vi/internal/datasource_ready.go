d2b2d0ad60c37cd88e794ff492fe0137b17fa25e
Dmitry Lopatin <dmitry.lopatin@flant.com>
2024-09-10 23:43:04 +0300

    fix after review
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/vi/internal/datasource_ready.go
+++ images/virtualization-artifact/pkg/controller/vi/internal/datasource_ready.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -74,27 +73,27 @@ func (h DatasourceReadyHandler) Handle(ctx context.Context, vi *virtv2.VirtualIm
 	case errors.Is(err, source.ErrSecretNotFound):
 		condition.Status = metav1.ConditionFalse
 		condition.Reason = vicondition.ContainerRegistrySecretNotFound
-		condition.Message = strings.ToTitle(err.Error())
+		condition.Message = service.CapitalizeFirstLetter(err.Error() + ".")
 		return reconcile.Result{}, nil
 	case errors.As(err, &source.ImageNotReadyError{}):
 		condition.Status = metav1.ConditionFalse
 		condition.Reason = vicondition.ImageNotReady
-		condition.Message = strings.ToTitle(err.Error())
+		condition.Message = service.CapitalizeFirstLetter(err.Error() + ".")
 		return reconcile.Result{}, nil
 	case errors.As(err, &source.ClusterImageNotReadyError{}):
 		condition.Status = metav1.ConditionFalse
 		condition.Reason = vicondition.ClusterImageNotReady
-		condition.Message = strings.ToTitle(err.Error())
+		condition.Message = service.CapitalizeFirstLetter(err.Error() + ".")
 		return reconcile.Result{}, nil
 	case errors.As(err, &source.VirtualDiskNotReadyError{}):
 		condition.Status = metav1.ConditionFalse
 		condition.Reason = vicondition.VirtualDiskNotReady
-		condition.Message = strings.ToTitle(err.Error())
+		condition.Message = service.CapitalizeFirstLetter(err.Error() + ".")
 		return reconcile.Result{}, nil
 	case errors.As(err, &source.VirtualDiskAttachedToRunningVMError{}):
 		condition.Status = metav1.ConditionFalse
 		condition.Reason = cvicondition.VirtualDiskNotReady
-		condition.Message = service.CapitalizeFirstLetter(err.Error())
+		condition.Message = service.CapitalizeFirstLetter(err.Error() + ".")
 		return reconcile.Result{}, nil
 	default:
 		return reconcile.Result{}, err
