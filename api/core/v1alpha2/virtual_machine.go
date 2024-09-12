6ac9ef1dbcc48be2a1d01be9fa7d84e366410c89
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2024-09-12 10:34:31 +0300

    core fraction madness
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- api/core/v1alpha2/virtual_machine.go
+++ api/core/v1alpha2/virtual_machine.go
@@ -161,7 +161,8 @@ type CPUSpec struct {
 	Cores int `json:"cores"`
 	// Guaranteed share of CPU that will be allocated to the VM. Specified as a percentage.
 	// +kubebuilder:default:="100%"
-	// +kubebuilder:validation:Enum:={"5%", "10%", "25%", "50%", "100%"}
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=100
 	CoreFraction string `json:"coreFraction,omitempty"`
 }
 
