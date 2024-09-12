801ccbcd11f13129db3188785fd221d344a8ad3b
Nikita korolev <nikita.korolev@flant.com>
2024-09-12 12:09:57 +0300

    chore(ci): add file as param
    Signed-off-by: Nikita korolev <nikita.korolev@flant.com>

--- tools/cve/scan-main.sh
+++ tools/cve/scan-main.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-report_file_name="$(date +%Y-%m-%d)-report.txt"
+# Copyright 2024 Flant JSC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+REPORT_FILE_NAME=$1
+if [[ -z $REPORT_FILE_NAME ]];then echo "file must be define";exit 1;fi
+# report_file_name="$(date +%Y-%m-%d)-report.txt"
 # module_tag=main
 module_tag=pr358
 
@@ -23,4 +39,4 @@ images=$(crane export dev-registry.deckhouse.io/sys/deckhouse-oss/modules/virtua
     trivy image ${image} -f table
 
   done <<< $(echo ${images} | jq -c .)
-} > "${report_file_name}"
+} > "${REPORT_FILE_NAME}"
