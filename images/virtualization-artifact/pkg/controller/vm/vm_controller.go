97179b1c600f8de603f976cec6ffb6a0e6df9b9e
yaroslavborbat <yaroslav.752@gmail.com>
2024-09-20 01:06:58 +0300

    fix
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

--- images/virtualization-artifact/pkg/controller/vm/vm_controller.go
+++ images/virtualization-artifact/pkg/controller/vm/vm_controller.go
@@ -27,7 +27,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 
 	"github.com/deckhouse/virtualization-controller/pkg/controller/ipam"
-	"github.com/deckhouse/virtualization-controller/pkg/controller/service"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/vm/internal"
 	"github.com/deckhouse/virtualization-controller/pkg/dvcr"
 	"github.com/deckhouse/virtualization-controller/pkg/logger"
@@ -54,7 +53,7 @@ func SetupController(
 		internal.NewClassHandler(client, recorder),
 		internal.NewIPAMHandler(ipam.New(), client, recorder),
 		internal.NewBlockDeviceHandler(client, recorder),
-		internal.NewProvisioningHandler(client, service.NewProvisioningValidator(client)),
+		internal.NewProvisioningHandler(client),
 		internal.NewAgentHandler(),
 		internal.NewFilesystemHandler(),
 		internal.NewPodHandler(client),
