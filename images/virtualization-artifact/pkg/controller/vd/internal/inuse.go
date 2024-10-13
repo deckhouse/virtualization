ac0fa17ea4025a577534ec2b7a60b12747a10fac
dmitry.lopatin <dmitry.lopatin@flant.com>
2024-10-13 14:52:59 +0300

    fix
    Signed-off-by: dmitry.lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/controller/vd/internal/inuse.go
+++ images/virtualization-artifact/pkg/controller/vd/internal/inuse.go
@@ -44,7 +44,6 @@ func NewInUseHandler(client client.Client) *InUseHandler {
 
 func (h InUseHandler) Handle(ctx context.Context, vd *virtv2.VirtualDisk) (reconcile.Result, error) {
 	log := logger.FromContext(ctx).With(logger.SlogHandler("inuse"))
-
 	inUseCondition, ok := service.GetCondition(vdcondition.InUseType, vd.Status.Conditions)
 	if !ok {
 		inUseCondition = metav1.Condition{
@@ -55,74 +54,64 @@ func (h InUseHandler) Handle(ctx context.Context, vd *virtv2.VirtualDisk) (recon
 		}
 	}
 
-	if inUseCondition.Status != metav1.ConditionTrue {
-		var inUsed bool
+	var inUsed bool
 
-		var viList virtv2.VirtualImageList
-		err := h.client.List(ctx, &viList, &client.ListOptions{
-			Namespace: vd.GetNamespace(),
-		})
+	var viList virtv2.VirtualImageList
+	err := h.client.List(ctx, &viList, &client.ListOptions{
+		Namespace: vd.GetNamespace(),
+	})
 
-		if err != nil {
-			log.Error(fmt.Sprintf("failed to list vi: %s", err))
-			return reconcile.Result{}, err
-		}
-
-		for _, vi := range viList.Items {
-			if vi.Spec.DataSource.Type != virtv2.DataSourceTypeObjectRef || vi.Spec.DataSource.ObjectRef == nil {
-				continue
-			}
-
-			if vi.Spec.DataSource.ObjectRef.Kind != virtv2.VirtualDiskKind || vi.Spec.DataSource.ObjectRef.Name != vd.GetName() {
-				continue
-			}
-
-			readyCondition, _ := service.GetCondition(vicondition.ReadyType, vi.Status.Conditions)
-			if readyCondition.Status != metav1.ConditionTrue {
-				inUsed = true
-			}
-		}
-
-		var cviList virtv2.ClusterVirtualImageList
-		err = h.client.List(ctx, &cviList, &client.ListOptions{})
-		if err != nil {
-			log.Error(fmt.Sprintf("failed to list cvi: %s", err))
-			return reconcile.Result{}, err
-		}
-
-		for _, cvi := range cviList.Items {
-			if cvi.Spec.DataSource.Type != virtv2.DataSourceTypeObjectRef || cvi.Spec.DataSource.ObjectRef == nil {
-				continue
-			}
-
-			if cvi.Spec.DataSource.ObjectRef.Kind != virtv2.VirtualDiskKind || cvi.Spec.DataSource.ObjectRef.Name != vd.GetName() && cvi.Spec.DataSource.ObjectRef.Namespace != vd.GetNamespace() {
-				continue
-			}
-
-			readyCondition, _ := service.GetCondition(cvicondition.ReadyType, cvi.Status.Conditions)
-			if readyCondition.Status != metav1.ConditionTrue {
-				inUsed = true
-			}
-		}
-
-		if inUsed {
-			inUseCondition.Status = metav1.ConditionTrue
-			inUseCondition.Reason = vdcondition.InUseForCreateImage
-			println("")
-			println("")
-			println("dlopatin inUseCondition.Reason = vdcondition.InUseForCreateImage")
-			println("")
-		} else if inUseCondition.Reason == vdcondition.InUseForCreateImage {
-			inUseCondition.Status = metav1.ConditionFalse
-			inUseCondition.Reason = vdcondition.NotUse
-			println("")
-			println("")
-			println("dlopatin inUseCondition.Reason = vdcondition.NotUse")
-			println("")
-		}
-
-		service.SetCondition(inUseCondition, &vd.Status.Conditions)
+	if err != nil {
+		log.Error(fmt.Sprintf("failed to list vi: %s", err))
+		return reconcile.Result{}, err
 	}
+
+	for _, vi := range viList.Items {
+		if vi.Spec.DataSource.Type != virtv2.DataSourceTypeObjectRef || vi.Spec.DataSource.ObjectRef == nil {
+			continue
+		}
+
+		if vi.Spec.DataSource.ObjectRef.Kind != virtv2.VirtualDiskKind || vi.Spec.DataSource.ObjectRef.Name != vd.GetName() {
+			continue
+		}
+
+		readyCondition, _ := service.GetCondition(vicondition.ReadyType, vi.Status.Conditions)
+		if readyCondition.Status != metav1.ConditionTrue {
+			inUsed = true
+		}
+	}
+
+	var cviList virtv2.ClusterVirtualImageList
+	err = h.client.List(ctx, &cviList, &client.ListOptions{})
+	if err != nil {
+		log.Error(fmt.Sprintf("failed to list cvi: %s", err))
+		return reconcile.Result{}, err
+	}
+
+	for _, cvi := range cviList.Items {
+		if cvi.Spec.DataSource.Type != virtv2.DataSourceTypeObjectRef || cvi.Spec.DataSource.ObjectRef == nil {
+			continue
+		}
+
+		if cvi.Spec.DataSource.ObjectRef.Kind != virtv2.VirtualDiskKind || cvi.Spec.DataSource.ObjectRef.Name != vd.GetName() && cvi.Spec.DataSource.ObjectRef.Namespace != vd.GetNamespace() {
+			continue
+		}
+
+		readyCondition, _ := service.GetCondition(cvicondition.ReadyType, cvi.Status.Conditions)
+		if readyCondition.Status != metav1.ConditionTrue {
+			inUsed = true
+		}
+	}
+
+	if inUsed {
+		inUseCondition.Status = metav1.ConditionTrue
+		inUseCondition.Reason = vdcondition.InUseForCreateImage
+	} else if inUseCondition.Reason == vdcondition.InUseForCreateImage {
+		inUseCondition.Status = metav1.ConditionFalse
+		inUseCondition.Reason = vdcondition.NotUse
+	}
+
+	service.SetCondition(inUseCondition, &vd.Status.Conditions)
 
 	return reconcile.Result{}, nil
 }
