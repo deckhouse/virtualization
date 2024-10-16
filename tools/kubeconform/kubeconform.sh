a097ef851af86a27644da71b9360213f85b9c1dd
Ivan Mikheykin <ivan.mikheykin@flant.com>
2024-10-16 17:34:05 +0300

    ++ fix exit code on errors, add copyright
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- tools/kubeconform/kubeconform.sh
+++ tools/kubeconform/kubeconform.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2024 Flant JSC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 if [[ ! -d ../../templates ]]; then
   echo "Error: Run this script from tools/kubeconform"
   exit 1
@@ -176,3 +190,7 @@ def regularError(msg): msg|test("could not find schema for")|not ;
   "     errors: " + ($errors|length|tostring),
   "    invalid: " + ($invalid|length|tostring)
 ] | flatten | join("\n")'
+
+exitCode=$(jq -r '[.resources[] | select(.status == "statusError" or .status == "statusInvalid")] | length' kubeconform-report.json)
+
+exit $exitCode
