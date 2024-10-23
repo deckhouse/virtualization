88c5d2bb2ba9077a874d86d6418613fdecb7dc8b
Yaroslav Borbat <86148689+yaroslavborbat@users.noreply.github.com>
2024-10-23 20:41:13 +0300

    Update images/virtualization-artifact/pkg/controller/kvbuilder/kvvm.go
    Co-authored-by: Ivan Mikheykin <ivan.mikheykin@flant.com>
    Signed-off-by: Yaroslav Borbat <86148689+yaroslavborbat@users.noreply.github.com>

--- images/virtualization-artifact/pkg/controller/kvbuilder/kvvm.go
+++ images/virtualization-artifact/pkg/controller/kvbuilder/kvvm.go
@@ -153,12 +153,12 @@ func (b *KVVM) SetNodeSelector(vmNodeSelector, classNodeSelector map[string]stri
 	b.Resource.Spec.Template.Spec.NodeSelector = selector
 }
 
-func (b *KVVM) SetTolerations(tolerations, classTolerations []corev1.Toleration) {
+func (b *KVVM) SetTolerations(vmTolerations, classTolerations []corev1.Toleration) {
 	tolerationsMap := make(map[string]corev1.Toleration)
 	for _, toleration := range classTolerations {
 		tolerationsMap[toleration.Key] = toleration
 	}
-	for _, toleration := range tolerations {
+	for _, toleration := range vmTolerations {
 		tolerationsMap[toleration.Key] = toleration
 	}
 	resultTolerations := make([]corev1.Toleration, 0, len(tolerationsMap))
