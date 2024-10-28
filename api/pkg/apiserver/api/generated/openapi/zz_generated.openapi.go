5e6e190ff8355a9fcc262e78d6bc19ba0e195b96
Yaroslav Borbat <86148689+yaroslavborbat@users.noreply.github.com>
2024-10-28 13:32:47 +0300

    feat(vmclass): add tolerations (#465)
    add tolerations
    ---------
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>
    Co-authored-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- api/pkg/apiserver/api/generated/openapi/zz_generated.openapi.go
+++ api/pkg/apiserver/api/generated/openapi/zz_generated.openapi.go
@@ -3445,6 +3445,20 @@ func schema_virtualization_api_core_v1alpha2_VirtualMachineClassSpec(ref common.
 							Ref:     ref("github.com/deckhouse/virtualization/api/core/v1alpha2.NodeSelector"),
 						},
 					},
+					"tolerations": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Tolerations are the same as `spec.tolerations` in the [Pod](https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/). These tolerations will be merged with tolerations specified in VirtualMachine resource. VirtualMachine tolerations have higher priority.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("k8s.io/api/core/v1.Toleration"),
+									},
+								},
+							},
+						},
+					},
 					"cpu": {
 						SchemaProps: spec.SchemaProps{
 							Default: map[string]interface{}{},
@@ -3469,7 +3483,7 @@ func schema_virtualization_api_core_v1alpha2_VirtualMachineClassSpec(ref common.
 			},
 		},
 		Dependencies: []string{
-			"github.com/deckhouse/virtualization/api/core/v1alpha2.CPU", "github.com/deckhouse/virtualization/api/core/v1alpha2.NodeSelector", "github.com/deckhouse/virtualization/api/core/v1alpha2.SizingPolicy"},
+			"github.com/deckhouse/virtualization/api/core/v1alpha2.CPU", "github.com/deckhouse/virtualization/api/core/v1alpha2.NodeSelector", "github.com/deckhouse/virtualization/api/core/v1alpha2.SizingPolicy", "k8s.io/api/core/v1.Toleration"},
 	}
 }
 
