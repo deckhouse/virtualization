946e77d666acd66382ab863586fbf29edd533333
yaroslavborbat <yaroslav.752@gmail.com>
2024-10-28 14:06:51 +0300

    fix
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

--- images/virtualization-artifact/pkg/controller/common/filter.go
+++ images/virtualization-artifact/pkg/controller/common/filter.go
@@ -22,15 +22,15 @@ import (
 
 type FilterFunc[T any] func(obj *T) (skip bool)
 
-func Filter[T any](objs []T, filters ...FilterFunc[T]) []T {
-	if len(filters) == 0 {
+func Filter[T any](objs []T, skips ...FilterFunc[T]) []T {
+	if len(skips) == 0 {
 		return slices.Clone(objs)
 	}
 	var filtered []T
 loop:
 	for _, o := range objs {
-		for _, f := range filters {
-			if f(&o) {
+		for _, skip := range skips {
+			if skip(&o) {
 				continue loop
 			}
 		}
