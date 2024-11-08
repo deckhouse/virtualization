a70674151f410e0259aa37d0ee0bf174f1b1f5d9
Daniil Antoshin <daniil.antoshin@flant.com>
2024-11-08 16:27:08 +0200

    fix: remove unused annotations
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- api/core/v1alpha2/cluster_virtual_image.go
+++ api/core/v1alpha2/cluster_virtual_image.go
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +kubebuilder:object:generate=true
-// +groupName=virtualization.deckhouse.io
 package v1alpha2
 
 import (
