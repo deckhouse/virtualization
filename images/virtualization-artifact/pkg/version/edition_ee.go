7d205b88f4d820d99a37a4f62e6b41d881f20de3
yaroslavborbat <yaroslav.752@gmail.com>
2024-11-12 11:02:52 +0300

    add license
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

--- images/virtualization-artifact/pkg/version/edition_ee.go
+++ images/virtualization-artifact/pkg/version/edition_ee.go
@@ -1,6 +1,11 @@
 //go:build EE
 // +build EE
 
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
 package version
 
 const edition = "EE"
