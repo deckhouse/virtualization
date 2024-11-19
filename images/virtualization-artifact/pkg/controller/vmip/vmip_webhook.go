70836b092ce3bd1c29f3255dbc9c0942bcc6e78f
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2024-11-19 12:24:11 +0300

    fix linter after merge
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- images/virtualization-artifact/pkg/controller/vmip/vmip_webhook.go
+++ images/virtualization-artifact/pkg/controller/vmip/vmip_webhook.go
@@ -27,8 +27,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
-	"github.com/deckhouse/virtualization-controller/pkg/controller/conditions"
 	"github.com/deckhouse/deckhouse/pkg/log"
+	"github.com/deckhouse/virtualization-controller/pkg/controller/conditions"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/service"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/vmip/internal/util"
 	"github.com/deckhouse/virtualization/api/core/v1alpha2"
