e3d3f5ba3ab018504248d82ee9cce56580a33748
yaroslavborbat <yaroslav.752@gmail.com>
2024-12-11 11:21:35 +0300

    fix
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

--- images/virtualization-artifact/pkg/controller/webhook/protect.go
+++ images/virtualization-artifact/pkg/controller/webhook/protect.go
@@ -5,8 +5,16 @@ import (
 	"slices"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	"kubevirt.io/api/clone"
 	virtcore "kubevirt.io/api/core"
+	"kubevirt.io/api/export"
+	"kubevirt.io/api/instancetype"
+	"kubevirt.io/api/migrations"
+	"kubevirt.io/api/pool"
+	"kubevirt.io/api/snapshot"
 	cdicore "kubevirt.io/containerized-data-importer-api/pkg/apis/core"
+	"kubevirt.io/containerized-data-importer-api/pkg/apis/forklift"
+	"kubevirt.io/containerized-data-importer-api/pkg/apis/upload"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
@@ -14,7 +22,15 @@ const ProtectResourcesPath = "/protect-resources"
 
 var defaultProtectGroups = []string{
 	virtcore.GroupName,
+	clone.GroupName,
+	export.GroupName,
+	instancetype.GroupName,
+	migrations.GroupName,
+	pool.GroupName,
+	snapshot.GroupName,
 	cdicore.GroupName,
+	upload.GroupName,
+	forklift.GroupName,
 }
 
 func newProtectHook(allowSA []string, groups []string) *protectHook {
