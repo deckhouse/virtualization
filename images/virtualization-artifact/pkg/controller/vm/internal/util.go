afe682a5c78f9d4d32996ec72e1407d11d05f93d
Daniil Antoshin <daniil.antoshin@flant.com>
2024-12-11 11:52:16 +0200

    refactor(vm): wrap KVMI ready reasons (#565)
    * feat(vm): wrap VM ready reasons
    
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- images/virtualization-artifact/pkg/controller/vm/internal/util.go
+++ images/virtualization-artifact/pkg/controller/vm/internal/util.go
@@ -133,6 +133,29 @@ var mapPhases = map[virtv1.VirtualMachinePrintableStatus]virtv2.MachinePhase{
 
 const kvvmEmptyPhase virtv1.VirtualMachinePrintableStatus = ""
 
+func getKVMIReadyReason(kvmiReason string) conditions.Stringer {
+	if r, ok := mapReasons[kvmiReason]; ok {
+		return r
+	}
+
+	if kvmiReason == "" {
+		return conditions.ReasonUnknown
+	}
+
+	return conditions.CommonReason(kvmiReason)
+}
+
+var mapReasons = map[string]vmcondition.Reason{
+	// PodTerminatingReason indicates on the Ready condition on the VMI if the underlying pod is terminating
+	virtv1.PodTerminatingReason: vmcondition.ReasonPodTerminatingReason,
+	// PodNotExistsReason indicates on the Ready condition on the VMI if the underlying pod does not exist
+	virtv1.PodNotExistsReason: vmcondition.ReasonPodNotExistsReason,
+	// PodConditionMissingReason indicates on the Ready condition on the VMI if the underlying pod does not report a Ready condition
+	virtv1.PodConditionMissingReason: vmcondition.ReasonPodConditionMissingReason,
+	// GuestNotRunningReason indicates on the Ready condition on the VMI if the underlying guest VM is not running
+	virtv1.GuestNotRunningReason: vmcondition.ReasonGuestNotRunningReason,
+}
+
 func isPodStartedError(phase virtv1.VirtualMachinePrintableStatus) bool {
 	return slices.Contains([]virtv1.VirtualMachinePrintableStatus{
 		virtv1.VirtualMachineStatusErrImagePull,
