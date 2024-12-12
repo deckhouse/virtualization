15f1af277785eb49909fa20f9fba99e365f5bfea
yaroslavborbat <yaroslav.752@gmail.com>
2024-12-12 12:34:38 +0300

    add
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

---
+++ images/virtualization-artifact/pkg/controller/moduleconfig/moduleconfig_webhook.go
@@ -0,0 +1,112 @@
+package moduleconfig
+
+import (
+	"context"
+	"fmt"
+	"net/netip"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/deckhouse/virtualization-controller/pkg/common/ip"
+	mcapi "github.com/deckhouse/virtualization-controller/pkg/controller/moduleconfig/api"
+	"github.com/deckhouse/virtualization-controller/pkg/logger"
+	virtv2 "github.com/deckhouse/virtualization/api/core/v1alpha2"
+)
+
+const virtualMachineCIDRs = "virtualMachineCIDRs"
+
+func SetupWebhookWithManager(mgr manager.Manager) error {
+	if err := builder.WebhookManagedBy(mgr).
+		For(&mcapi.ModuleConfig{}).
+		WithValidator(NewValidator(mgr.GetClient())).
+		Complete(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Validator struct {
+	client client.Client
+}
+
+func NewValidator(client client.Client) *Validator {
+	return &Validator{
+		client: client,
+	}
+}
+
+func (v *Validator) ValidateCreate(ctx context.Context, _ runtime.Object) (admission.Warnings, error) {
+	err := fmt.Errorf("misconfigured webhook rules: create operation not implemented")
+	logger.FromContext(ctx).Error("Ensure the correctness of ValidatingWebhookConfiguration", "err", err)
+	return nil, nil
+}
+
+func (v *Validator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	oldMC, ok := oldObj.(*mcapi.ModuleConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected an old ModuleConfig but got a %T", newObj)
+	}
+
+	newMC, ok := newObj.(*mcapi.ModuleConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected a new ModuleConfig but got a %T", newObj)
+	}
+
+	oldCIDRs := oldMC.Spec.Settings[virtualMachineCIDRs].([]string)
+	newCIDRs := newMC.Spec.Settings[virtualMachineCIDRs].([]string)
+
+	var validateCIDRs []string
+
+loop:
+	for _, oldCIDR := range oldCIDRs {
+		for _, newCIDR := range newCIDRs {
+			if oldCIDR == newCIDR {
+				continue loop
+			}
+		}
+		validateCIDRs = append(validateCIDRs, oldCIDR)
+	}
+
+	if len(validateCIDRs) != 0 {
+		return nil, nil
+	}
+
+	leases := &virtv2.VirtualMachineIPAddressLeaseList{}
+	if err := v.client.List(ctx, leases); err != nil {
+		return nil, fmt.Errorf("failed to list VirtualMachineIPAddressLeases: %w", err)
+	}
+
+	parseCIDRs := make([]netip.Prefix, len(validateCIDRs))
+	for i, validateCIDR := range validateCIDRs {
+		parsedCIDR, err := netip.ParsePrefix(validateCIDR)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse CIDR %s: %w", validateCIDR, err)
+		}
+		parseCIDRs[i] = parsedCIDR
+	}
+
+	for _, lease := range leases.Items {
+		leaseIP, err := netip.ParseAddr(ip.LeaseNameToIP(lease.Name))
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse lease ip: %w", err)
+		}
+		for _, CIDR := range parseCIDRs {
+			if CIDR.Contains(leaseIP) {
+				return nil, fmt.Errorf("CIDR %q is in use by one or more IP addresses", CIDR)
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func (v *Validator) ValidateDelete(ctx context.Context, _ runtime.Object) (admission.Warnings, error) {
+	err := fmt.Errorf("misconfigured webhook rules: delete operation not implemented")
+	logger.FromContext(ctx).Error("Ensure the correctness of ValidatingWebhookConfiguration", "err", err)
+	return nil, nil
+}
