13c451b2f59c78b96caaa1b887e673fb952558b9
yaroslavborbat <yaroslav.752@gmail.com>
2024-12-12 17:42:18 +0300

    add hook
    Signed-off-by: yaroslavborbat <yaroslav.752@gmail.com>

---
+++ hooks/mc-labeler.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#
+# Copyright 2024 Flant JSC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Callable
+from deckhouse import hook
+from lib.hooks.hook import Hook
+import common
+
+
+class ModuleConfigLabelHook(Hook):
+    KIND="ModuleConfig"
+    API_VERSION="deckhouse.io/v1alpha1"
+    SNAPSHOT_NAME = "module-config-label"
+    LABEL = "moduleconfig.deckhouse.io/name"
+
+    def __init__(self, module_name: str):
+        self.module_name = module_name
+
+    def generate_config(self) -> dict:
+        return {
+            "configVersion": "v1",
+            "kubernetes": [
+                {
+                    "name": self.SNAPSHOT_NAME,
+                    "apiVersion": self.API_VERSION,
+                    "kind": self.KIND,
+                    "nameSelector": {
+                        "matchNames": [self.module_name]
+                    },
+                    "group": "main",
+                    "jqFilter": '{"labels": .metadata.labels}',
+                    "queue": f"/modules/{self.module_name}/{self.SNAPSHOT_NAME}",
+                    "keepFullObjectsInMemory": False
+                },
+            ]
+        }
+
+
+    def reconcile(self) -> Callable[[hook.Context], None]:
+        def r(ctx: hook.Context):
+            labels = {}
+            if (snaps := ctx.snapshots.get(self.SNAPSHOT_NAME, [])) and len(snaps) > 0:
+                labels = snaps[0]["filterResult"]["labels"]
+
+            if labels.get(self.LABEL, "") == self.module_name:
+                return
+
+            ctx.kubernetes.merge_patch(
+                kind=self.KIND,
+                name=self.module_name,
+                patch={
+                    "metadata": {
+                        "labels": {
+                            self.LABEL: self.module_name
+                        }
+                    }
+                },
+                apiVersion=self.API_VERSION
+            )
+        return r
+
+
+if __name__ == "__main__":
+    h = ModuleConfigLabelHook(common.MODULE_NAME)
+    h.run()
