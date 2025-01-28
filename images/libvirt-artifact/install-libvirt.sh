5d55cfa848cf0de4b0682320f21c4846d0551e2b
Nikita Korolev <nikita.korolev@flant.com>
2025-01-29 02:35:46 +0300

    qemu,libvirt: fix regex in install script
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- images/libvirt-artifact/install-libvirt.sh
+++ images/libvirt-artifact/install-libvirt.sh
@@ -601,7 +601,7 @@ copy_file() {
 
     # Copy the file
     # install -s "$SOURCE_PATH" "$DEST_BASE$dest_dir"
-    if ! [[ "$SOURCE_PATH" =~ \.(txt|log|sh|service|conf|xml|target|socket) ]];then
+    if ! [[ "$SOURCE_PATH" =~ \.(txt|log|sh|service|conf|xml|target|socket|bin|json|img|png|fd|dtb|rom) ]];then
         strip "$SOURCE_PATH"
     fi
     cp -p "$SOURCE_PATH" "$DEST_BASE$dest_dir"
