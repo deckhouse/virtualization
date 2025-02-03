a005749af08b4aaf89dd9648e7ac2a9c0b2fc66b
Roman Sysoev <36233932+hardcoretime@users.noreply.github.com>
2025-02-03 14:07:07 +0300

    test(ci): fix e2e namespace cleanup operation (#682)
    Signed-off-by: Roman Sysoev <roman.sysoev@flant.com>

--- tests/e2e/tests_suite_test.go
+++ tests/e2e/tests_suite_test.go
@@ -160,7 +160,7 @@ func Cleanup() []error {
 			continue
 		}
 		res := kubectl.Delete(kc.DeleteOptions{
-			Filename:       []string{conf.Namespace},
+			Filename:       []string{namespace},
 			IgnoreNotFound: true,
 			Resource:       kc.ResourceNamespace,
 		})
