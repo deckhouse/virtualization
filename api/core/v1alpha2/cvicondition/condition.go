58dabdc900b3f3588752f6f5eab1a6c90d9d6010
dmitry.lopatin <dmitry.lopatin@flant.com>
2025-02-12 14:58:00 +0300

    fix(vd): refactor inuse handler
    Signed-off-by: dmitry.lopatin <dmitry.lopatin@flant.com>

--- api/core/v1alpha2/cvicondition/condition.go
+++ api/core/v1alpha2/cvicondition/condition.go
@@ -56,8 +56,8 @@ const (
 	ClusterImageNotReady DatasourceReadyReason = "ClusterImageNotReady"
 	// VirtualDiskNotReady indicates that the `VirtualDisk` datasource is not ready, which prevents the import process from starting.
 	VirtualDiskNotReady DatasourceReadyReason = "VirtualDiskNotReady"
-	// VirtualDiskInUseInRunningVirtualMachine indicates that the `VirtualDisk` attached to running `VirtualMachine`
-	VirtualDiskInUseInRunningVirtualMachine DatasourceReadyReason = "VirtualDiskInUseInRunningVirtualMachine"
+	// VirtualDiskNotReadyForUse indicates that the `VirtualDisk` not ready for use, which prevents the import process from starting.
+	VirtualDiskNotReadyForUse DatasourceReadyReason = "VirtualDiskNotReadyForUse"
 
 	// WaitForUserUpload indicates that the `ClusterVirtualImage` is waiting for the user to upload a datasource for the import process to continue.
 	WaitForUserUpload ReadyReason = "WaitForUserUpload"
