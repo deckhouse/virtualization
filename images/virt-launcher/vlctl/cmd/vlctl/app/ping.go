c06b900e069c07bef312fbd282fc9bcd6c9133cf
Yaroslav Borbat <yaroslav.borbat@flant.com>
2025-03-03 20:52:53 +0300

    impl
    Signed-off-by: Yaroslav Borbat <yaroslav.borbat@flant.com>

---
+++ images/virt-launcher/vlctl/cmd/vlctl/app/ping.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func NewPingCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "ping",
+		Short: "Ping virt-launcher",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			baseOpts := BaseOptionsFromCommand(cmd)
+			return runPingCommand(baseOpts)
+		},
+	}
+}
+
+func runPingCommand(opts BaseOptions) error {
+	if err := opts.Validate(); err != nil {
+		return err
+	}
+
+	client, err := opts.Client()
+	if err != nil {
+		return fmt.Errorf("failed to create client: %w", err)
+	}
+
+	err = client.Ping()
+	if err != nil {
+		return fmt.Errorf("failed to ping: %w", err)
+	}
+
+	_, err = fmt.Fprintln(os.Stdout, "PONG")
+
+	return err
+}
