6d69988323cbfa3b8dee915e2b4c4cbfaf82d812
Roman Sysoev <roman.sysoev@flant.com>
2025-03-04 04:14:26 +0300

    test(vm): refactor affinity and toleration test case
    - Improve Ginkgo contexts
    - Add node selector configuration case
    - Add node affinity configuration case
    - Add virtual machine and pod affinity configuration case
    - Add virtual machine and pod anti-affinity configuration case
    
    Signed-off-by: Roman Sysoev <roman.sysoev@flant.com>

--- tests/e2e/util_test.go
+++ tests/e2e/util_test.go
@@ -147,7 +147,7 @@ func WaitResource(resource kc.Resource, name, waitFor string, timeout time.Durat
 	Expect(res.Error()).NotTo(HaveOccurred(), "wait failed %s %s/%s.\n%s", resource, conf.Namespace, name, res.StdErr())
 }
 
-func PatchResource(resource kc.Resource, name string, patch *kc.JsonPatch) {
+func PatchResource(resource kc.Resource, name string, patch []*kc.JsonPatch) {
 	GinkgoHelper()
 	res := kubectl.PatchResource(resource, name, kc.PatchOptions{
 		Namespace: conf.Namespace,
