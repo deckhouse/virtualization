8687545fbf62f4e71f3c3b86a3b6288177bad65d
Daniil Antoshin <daniil.antoshin@flant.com>
2025-03-19 09:09:36 +0200

    feat: add virtualization control event logging
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

---
+++ images/virtualization-artifact/pkg/audit/events/v12n_module_control.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package events
+
+import (
+	"k8s.io/apiserver/pkg/apis/audit"
+	"k8s.io/client-go/tools/cache"
+)
+
+type NewV12NModuleControlOptions struct {
+	NodeInformer cache.Indexer
+	PodInformer  cache.Indexer
+}
+
+func NewV12NModuleControl(options NewV12NModuleControlOptions) *V12NModuleControl {
+	return &V12NModuleControl{
+		nodeInformer: options.NodeInformer,
+		podInformer:  options.PodInformer,
+	}
+}
+
+type V12NModuleControl struct {
+	podInformer  cache.Indexer
+	nodeInformer cache.Indexer
+}
+
+func (m *V12NModuleControl) IsMatched(event *audit.Event) bool {
+	if event.ObjectRef == nil || event.Stage != audit.StageResponseComplete {
+		return false
+	}
+
+	if event.ObjectRef.Resource == "moduleconfigs" && event.ObjectRef.Name == "virtualization" {
+		return true
+	}
+
+	return false
+}
+
+func (m *V12NModuleControl) Log(event *audit.Event) error {
+	eventLog := NewV12NEventLog(event)
+	eventLog.Type = "Virtualization control"
+
+	if event.Verb == "create" {
+		eventLog.Name = "Module creation"
+		eventLog.Level = "info"
+
+	} else {
+		eventLog.Name = "Module deletion"
+		eventLog.Level = "warn"
+	}
+
+	return eventLog.Log()
+}
