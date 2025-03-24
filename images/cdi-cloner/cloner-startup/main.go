88ffb6cd9cccdff736a6ec090a563be1f59e4412
Nikita Korolev <nikita.korolev@flant.com>
2025-03-24 18:05:09 +0300

    rm unnecessary code
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- images/cdi-cloner/cloner-startup/main.go
+++ images/cdi-cloner/cloner-startup/main.go
@@ -36,6 +36,65 @@ func getEnv(key string) (string, error) {
 	return value, nil
 }
 
+func getBlockSize(device string) (uint64, error) {
+	fd, err := unix.Open(device, unix.O_RDONLY, 0)
+	if err != nil {
+		return 0, fmt.Errorf("open device: %w", err)
+	}
+	defer unix.Close(fd)
+	var (
+		size         uint64
+		BLKGETSIZE64 = 0x80081272 // BLKGETSIZE64 ioctl command
+	)
+
+	// Use BLKGETSIZE64 (0x80081272)
+	_, _, errno := unix.Syscall(
+		syscall.SYS_IOCTL,
+		uintptr(fd),
+		uintptr(BLKGETSIZE64),
+		uintptr(unsafe.Pointer(&size)),
+	)
+	if errno != 0 {
+		return 0, fmt.Errorf("ioctl BLKGETSIZE64: %w", errno)
+	}
+	return size, nil
+}
+
+// getDirectorySize calculates directory size using Go's filepath.Walk
+func getDirectorySize(path string, preallocate bool) (uint64, error) {
+	var total uint64
+	err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+		if preallocate {
+			total += uint64(info.Size())
+		} else {
+			if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+				total += uint64(stat.Blocks * 512) // 512-byte blocks
+			}
+		}
+		return nil
+	})
+	return total, err
+}
+
+func runCloner(contentType string, uploadBytes uint64, mountPoint string) {
+	cmd := exec.Command("/usr/bin/cdi-cloner",
+		"-v=3",
+		"-alsologtostderr",
+		"-content-type="+contentType,
+		"-upload-bytes="+strconv.FormatUint(uploadBytes, 10),
+		"-mount="+mountPoint,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running cdi-cloner: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	volumeMode, err := getEnv("VOLUME_MODE")
 	if err != nil {
@@ -83,6 +142,7 @@ func main() {
 		currentDir, err := os.Getwd()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error getting current directory: %v\n", err)
+			defer os.Chdir(currentDir)
 			os.Exit(1)
 		}
 		defer os.Chdir(currentDir)
@@ -104,82 +164,5 @@ func main() {
 			os.Exit(1)
 		}
 		runCloner("filesystem-clone", uploadBytes, mountPoint)
-	}
-}
-
-func getBlockSize(device string) (uint64, error) {
-	fd, err := unix.Open(device, unix.O_RDONLY, 0)
-	if err != nil {
-		return 0, fmt.Errorf("open device: %w", err)
-	}
-	defer unix.Close(fd)
-	var (
-		size         uint64
-		BLKGETSIZE64 = 0x80081272 // BLKGETSIZE64 ioctl command
-	)
-
-	// Use BLKGETSIZE64 (0x80081272)
-	_, _, errno := unix.Syscall(
-		syscall.SYS_IOCTL,
-		uintptr(fd),
-		uintptr(BLKGETSIZE64),
-		uintptr(unsafe.Pointer(&size)),
-	)
-	if errno != 0 {
-		return 0, fmt.Errorf("ioctl BLKGETSIZE64: %w", errno)
-	}
-	return size, nil
-}
-
-// getBlockSize uses ioctl to get block device size
-// func getBlockSize(device string) (uint64, error) {
-// 	fd, err := syscall.Open(device, syscall.O_RDONLY, 0)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	defer syscall.Close(fd)
-
-// 	var size uint64
-// 	// BLKGETSIZE64 ioctl command (0x80081272)
-// 	// if err := syscall.Ioctl(fd, 0x80081272, uintptr(unsafe.Pointer(&size))); err != nil {
-// 	if err := unix.Ioctl(fd, unix.BLK, uintptr(unsafe.Pointer(&size))); err != nil {
-// 		// if err := unix.Ioctl(fd, 0x80081272, uintptr(unsafe.Pointer(&size))); err != nil {
-// 		return 0, err
-// 	}
-// 	return size, nil
-// }
-
-// getDirectorySize calculates directory size using Go's filepath.Walk
-func getDirectorySize(path string, preallocate bool) (uint64, error) {
-	var total uint64
-	err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
-		if err != nil || info.IsDir() {
-			return err
-		}
-		if preallocate {
-			total += uint64(info.Size())
-		} else {
-			if stat, ok := info.Sys().(*syscall.Stat_t); ok {
-				total += uint64(stat.Blocks * 512) // 512-byte blocks
-			}
-		}
-		return nil
-	})
-	return total, err
-}
-
-func runCloner(contentType string, uploadBytes uint64, mountPoint string) {
-	cmd := exec.Command("/usr/bin/cdi-cloner",
-		"-v=3",
-		"-alsologtostderr",
-		"-content-type="+contentType,
-		"-upload-bytes="+strconv.FormatUint(uploadBytes, 10),
-		"-mount="+mountPoint,
-	)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error running cdi-cloner: %v\n", err)
-		os.Exit(1)
 	}
 }
