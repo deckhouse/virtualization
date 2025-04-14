a1d63ce3ebeb84be68955435a2c8aa654a6b76a6
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2025-04-14 11:34:35 +0300

    fix merge conflicts
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- images/virtualization-artifact/pkg/controller/indexer/cvi_indexer.go
+++ images/virtualization-artifact/pkg/controller/indexer/cvi_indexer.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Flant JSC
+Copyright 2025 Flant JSC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package indexer
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -66,30 +67,6 @@ func IndexCVIByVIDataSourceIndexerFunc(object client.Object) []string {
 
 	return []string{cvi.Spec.DataSource.ObjectRef.Name}
 }
-/*
-Copyright 2025 Flant JSC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package indexer
-
-import (
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	virtv2 "github.com/deckhouse/virtualization/api/core/v1alpha2"
-)
 
 func IndexCVIByVDSnapshot() (obj client.Object, field string, extractValue client.IndexerFunc) {
 	return &virtv2.ClusterVirtualImage{}, IndexFieldCVIByVDSnapshot, func(object client.Object) []string {
