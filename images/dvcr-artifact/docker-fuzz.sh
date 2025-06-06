62d80a6e034b4a80012cf2b6974325fe86306fd0
Daniil Antoshin <daniil.antoshin@flant.com>
2025-06-06 10:16:59 +0200

    +
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- images/dvcr-artifact/docker-fuzz.sh
+++ images/dvcr-artifact/docker-fuzz.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Simple Docker fuzzing script for uploader package
-
 IMAGE_NAME="uploader-fuzz"
 DOCKERFILE="Dockerfile.fuzz"
 DEFAULT_FUZZ_TIME="2m"
