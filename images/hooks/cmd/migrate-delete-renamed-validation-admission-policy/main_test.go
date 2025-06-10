7cc329e2651c18608f6296fac3d5ffb921bbe345
Nikita Korolev <nikita.korolev@flant.com>
2025-06-10 13:22:37 +0300

    add tests, 2 from 6 fail
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- images/hooks/cmd/migrate-delete-renamed-validation-admission-policy/main_test.go
+++ images/hooks/cmd/migrate-delete-renamed-validation-admission-policy/main_test.go
@@ -18,17 +18,19 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/deckhouse/deckhouse/pkg/log"
 	"github.com/deckhouse/module-sdk/pkg"
 	"github.com/deckhouse/module-sdk/testing/mock"
 
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestMigrateDeleteRenamedValidationAadmissionPolicy(t *testing.T) {
@@ -36,38 +38,49 @@ func TestMigrateDeleteRenamedValidationAadmissionPolicy(t *testing.T) {
 	RunSpecs(t, "MigrateDeleteRenamedValidationAadmissionPolicy Suite")
 }
 
-var _ = Describe("TST vap", func() {
+var _ = FDescribe("Test ValidatingAdmissionPolicy/ValidatingAdmissionPolicyBinding", func() {
 	var (
 		dc        *mock.DependencyContainerMock
 		snapshots *mock.SnapshotsMock
 	)
 
-	testDataVap := admissionregistrationv1.ValidatingAdmissionPolicy{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ValidatingAdmissionPolicy",
-			APIVersion: "admissionregistration.k8s.io/v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-vap",
-			Labels: map[string]string{
-				"test": "test",
-			},
-		},
+	// testDataVap := admissionregistrationv1.ValidatingAdmissionPolicy{
+	// 	TypeMeta: metav1.TypeMeta{
+	// 		Kind:       "ValidatingAdmissionPolicy",
+	// 		APIVersion: "admissionregistration.k8s.io/v1",
+	// 	},
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name: "test-vap",
+	// 		Labels: map[string]string{
+	// 			"test": "test",
+	// 		},
+	// 	},
+	// }
+
+	setSnapshots := func(snapPolicy, snapBinding []pkg.Snapshot) {
+		snapshots.GetMock.When(POLICY_SNAPSHOT_NAME).Then(snapPolicy)
+		snapshots.GetMock.When(BINDING_SNAPSHOT_NAME).Then(snapBinding)
 	}
 
-	setSnapshots := func(snaps ...pkg.Snapshot) {
-		snapshots.GetMock.When(POLICY_SNAPSHOT_NAME).Then(snaps...)
-	}
-
-	newSnapshot := func(data admissionregistrationv1.ValidatingAdmissionPolicy) pkg.Snapshot {
+	newSnapshotPolicy := func(labels map[string]string) pkg.Snapshot {
 		return mock.NewSnapshotMock(GinkgoT()).UnmarshalToMock.Set(func(v any) (err error) {
 			data, ok := v.(*admissionregistrationv1.ValidatingAdmissionPolicy)
 			Expect(ok).To(BeTrue())
+			data.Name = POLICY_SNAPSHOT_NAME
 			data.Kind = "ValidatingAdmissionPolicy"
 			data.APIVersion = "admissionregistration.k8s.io/v1"
-			data.Labels = map[string]string{
-				"test": "test",
-			}
+			data.Labels = labels
+			return nil
+		})
+	}
+	newSnapshotBinding := func(labels map[string]string) pkg.Snapshot {
+		return mock.NewSnapshotMock(GinkgoT()).UnmarshalToMock.Set(func(v any) (err error) {
+			data, ok := v.(*admissionregistrationv1.ValidatingAdmissionPolicyBinding)
+			Expect(ok).To(BeTrue())
+			data.Name = BINDING_SNAPSHOT_NAME
+			data.Kind = "ValidatingAdmissionPolicyBinding"
+			data.APIVersion = "admissionregistration.k8s.io/v1"
+			data.Labels = labels
 			return nil
 		})
 	}
@@ -79,50 +92,103 @@ var _ = Describe("TST vap", func() {
 			Logger:    log.NewNop(),
 		}
 	}
-	It("Test", func() {
-		setSnapshots(newSnapshot(testDataVap))
+
+	BeforeEach(func() {
+		dc = mock.NewDependencyContainerMock(GinkgoT())
+
+		dc.GetK8sClientMock.Set(func(options ...pkg.KubernetesOption) (pkg.KubernetesClient, error) {
+			return mock.NewKubernetesClientMock(GinkgoT()), nil
+		})
+		snapshots = mock.NewSnapshotsMock(GinkgoT())
+	})
+
+	AfterEach(func() {
+		dc = nil
+		snapshots = nil
+	})
+
+	It("test with labels test:test", func() {
+		setSnapshots(
+			[]pkg.Snapshot{newSnapshotPolicy(map[string]string{"test": "test"})},
+			[]pkg.Snapshot{newSnapshotBinding(map[string]string{"test": "test"})})
 		Expect(reconcile(context.Background(), newInput())).To(Succeed())
 	})
+	It("test with labels app.kubernetes.io/managed-by:\"\"", func() {
+		setSnapshots(
+			[]pkg.Snapshot{newSnapshotPolicy(map[string]string{managed_by_label: managed_by_label_value})},
+			[]pkg.Snapshot{newSnapshotBinding(map[string]string{managed_by_label: managed_by_label_value})})
+		Expect(reconcile(context.Background(), newInput())).To(Succeed())
+	})
+
+	It("should failed when get k8s client", func() {
+		clientErr := errors.New("client error")
+		dc.GetK8sClientMock.Set(func(options ...pkg.KubernetesOption) (pkg.KubernetesClient, error) {
+			return nil, clientErr
+		})
+		setSnapshots(
+			[]pkg.Snapshot{newSnapshotPolicy(map[string]string{"test": "test"})},
+			[]pkg.Snapshot{newSnapshotBinding(map[string]string{"test": "test"})})
+		err := reconcile(context.Background(), newInput())
+		Expect(err).To(MatchError(clientErr))
+	})
+
+	It(fmt.Sprintf("sohuld skip vap %s", POLICY_SNAPSHOT_NAME), func() {
+		setSnapshots(
+			[]pkg.Snapshot{newSnapshotPolicy(map[string]string{"test": "test"})},
+			[]pkg.Snapshot{newSnapshotBinding(map[string]string{"test": "test"})})
+		Expect(reconcile(context.Background(), newInput())).To(Succeed())
+
+		dc.GetK8sClientMock.Set(func(options ...pkg.KubernetesOption) (pkg.KubernetesClient, error) {
+			c := mock.NewKubernetesClientMock(GinkgoT())
+			c.DeleteMock.Set(
+				func(ctx context.Context, obj client.Object, opts ...client.DeleteOption) (err error) {
+					vap, ok := obj.(*admissionregistrationv1.ValidatingAdmissionPolicy)
+					Expect(ok).To(BeTrue())
+					Expect(vap.Name).To(Equal(POLICY_SNAPSHOT_NAME))
+					return nil
+				})
+			return c, nil
+		})
+
+		// setSnapshots(
+		// 	[]pkg.Snapshot{newSnapshotPolicy(map[string]string{"test": "test"})},
+		// 	[]pkg.Snapshot{newSnapshotBinding(map[string]string{"test": "test"})})
+		// Expect(reconcile(context.Background(), newInput())).To(Succeed())
+	})
+	It(fmt.Sprintf("sohuld skip vap %s", BINDING_SNAPSHOT_NAME), func() {
+		setSnapshots(
+			[]pkg.Snapshot{newSnapshotPolicy(map[string]string{"test": "test"})},
+			[]pkg.Snapshot{newSnapshotBinding(map[string]string{"test": "test"})})
+		Expect(reconcile(context.Background(), newInput())).To(Succeed())
+
+		dc.GetK8sClientMock.Set(func(options ...pkg.KubernetesOption) (pkg.KubernetesClient, error) {
+			c := mock.NewKubernetesClientMock(GinkgoT())
+			c.DeleteMock.Set(
+				func(ctx context.Context, obj client.Object, opts ...client.DeleteOption) (err error) {
+					vap, ok := obj.(*admissionregistrationv1.ValidatingAdmissionPolicy)
+					Expect(ok).To(BeTrue())
+					Expect(vap.Name).To(Equal(BINDING_SNAPSHOT_NAME))
+					return nil
+				})
+			return c, nil
+		})
+	})
+	It(fmt.Sprintf("sohuld delete vap %s with labels", BINDING_SNAPSHOT_NAME), func() {
+		setSnapshots(
+			[]pkg.Snapshot{newSnapshotPolicy(map[string]string{managed_by_label: managed_by_label_value})},
+			[]pkg.Snapshot{newSnapshotBinding(map[string]string{managed_by_label: managed_by_label_value})})
+		Expect(reconcile(context.Background(), newInput())).To(Succeed())
+
+		dc.GetK8sClientMock.Set(func(options ...pkg.KubernetesOption) (pkg.KubernetesClient, error) {
+			c := mock.NewKubernetesClientMock(GinkgoT())
+			c.DeleteMock.Set(
+				func(ctx context.Context, obj client.Object, opts ...client.DeleteOption) (err error) {
+					vap, ok := obj.(*admissionregistrationv1.ValidatingAdmissionPolicy)
+					Expect(ok).To(BeTrue())
+					Expect(vap.Name).To(Equal(BINDING_SNAPSHOT_NAME))
+					return nil
+				})
+			return c, nil
+		})
+	})
 })
-
-// newSnapshot := func(testDataVap *admissionregistrationv1.ValidatingAdmissionPolicy) *pkg.Snapshot {
-// 	return mock.NewSnapshotMock(GinkgoT()).UnmarshalToMock.Set(func(v any) (err error) {
-// 		data, ok := v.(*admissionregistrationv1.ValidatingAdmissionPolicy)
-// 		Expect(ok).To(BeTrue())
-
-// 		*data = *testDataVap
-// 		return nil
-// 	})
-// }
-
-// var _ = Describe("DeleteVAP", func() {
-// 	var (
-// 		ctx        = testutil.ContextBackgroundWithNoOpLogger()
-// 		fakeClient client.WithWatch
-// 	)
-
-// 	BeforeEach(func() {
-// 		fakeClient = nil
-// 	})
-
-// 	newSnapshot := func(passwordRW, salt, htpasswd string) pkg.Snapshot {
-// 		return mock.NewSnapshotMock(GinkgoT()).UnmarshalToMock.Set(func(v any) (err error) {
-// 			data, ok := v.(*dvcrSecretData)
-// 			Expect(ok).To(BeTrue())
-
-// 			data.PasswordRW = passwordRW
-// 			data.Salt = salt
-// 			data.Htpasswd = htpasswd
-// 			return nil
-// 		})
-// 	}
-
-// 	newInput := func() *pkg.HookInput {
-// 		return &pkg.HookInput{
-// 			Snapshots: snapshots,
-// 			Values:    values,
-// 			DC:        dc,
-// 			Logger:    log.NewNop(),
-// 		}
-// 	}
-// })
