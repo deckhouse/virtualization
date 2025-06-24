221ef666ad43c41106af97b56e7584bf88d78548
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2025-06-24 18:29:06 +0300

    fix problems)
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- images/virtualization-artifact/pkg/controller/cvi/internal/life_cycle.go
+++ images/virtualization-artifact/pkg/controller/cvi/internal/life_cycle.go
@@ -56,14 +56,21 @@ func (h LifeCycleHandler) Handle(ctx context.Context, cvi *virtv2.ClusterVirtual
 
 	if cvi.DeletionTimestamp != nil {
 		// It is necessary to update this condition in order to use this image as a data source.
+		cb := conditions.NewConditionBuilder(cvicondition.ReadyType).Generation(cvi.Generation)
+
 		if readyCondition.Status == metav1.ConditionTrue {
-			cb := conditions.NewConditionBuilder(cvicondition.ReadyType).Generation(cvi.Generation).
+			cb.
 				Status(metav1.ConditionTrue).
 				Reason(cvicondition.Ready).
 				Message("")
-			conditions.SetCondition(cb, &cvi.Status.Conditions)
+		} else {
+			cb.
+				Status(readyCondition.Status).
+				Reason(conditions.ReasonUnknown).
+				Message("")
 		}
 
+		conditions.SetCondition(cb, &cvi.Status.Conditions)
 		cvi.Status.Phase = virtv2.ImageTerminating
 		return reconcile.Result{}, nil
 	}
