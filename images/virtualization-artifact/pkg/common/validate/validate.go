455c495442ab801f8c9ce45deffde6cce1921b4c
Dmitry Lopatin <dmitry.lopatin@flant.com>
2025-06-25 09:29:53 +0300

    fix(api): fix validating name in webhook
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- images/virtualization-artifact/pkg/common/validate/validate.go
+++ images/virtualization-artifact/pkg/common/validate/validate.go
@@ -21,14 +21,9 @@ package validate
 // We add prefix "vd-" for the vd name, so max len reduced to 60.
 const MaxDiskNameLen = 60
 
-// MaxVirtualImageOnDVCRNameLen determines the max len of vi on dvcr.
-// This limit ensures compatibility with hotplug operations
-const MaxVirtualImageOnDVCRNameLen = 37
-
-// MaxVirtualImageOnPVCNameLen determines the max len of vi on pvc.
-// Disk and volume name in kubevirt can be a valid container name (len 63) since disk name can become a container name which will fail to schedule if invalid.
-// We and kubevirt add prefixes "vi-", "volume" and suffix "-init", so max len reduced to 49.
-const MaxVirtualImageOnPVCNameLen = 49
+// MaxVirtualImageNameLen determines the max len of vi on dvcr.
+// This limitation is reportedly associated with mounting image to virtual machine as hotplug.
+const MaxVirtualImageNameLen = 37
 
 // MaxClusterVirtualImageNameLen determines the max len of cvi.
 // Disk and volume name in kubevirt can be a valid container name (len 63) since disk name can become a container name which will fail to schedule if invalid.
@@ -36,5 +31,5 @@ const MaxVirtualImageOnPVCNameLen = 49
 const MaxClusterVirtualImageNameLen = 36
 
 // MaxVirtualMachineNameLen determines the max len of vm.
-// The name of the VirtualMachine can be valid with a length of up to 63 characters, as exceeding this limit may cause a failure in creating internal resources.
+// The limitation is reportedly associated with the PodDisruptionBudget resource, which has a label containing the virtual machine's name, and the label's value cannot exceed 63 characters.
 const MaxVirtualMachineNameLen = 63
