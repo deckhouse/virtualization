f6c8c1af9f342b6a80b84c49e27a64d9f8946cad
Nikita Korolev <nikita.korolev@flant.com>
2025-06-30 17:14:41 +0300

    fix werf.inc.yaml, add todo
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- images/hooks/cmd/migrate-delete-renamed-validation-admission-policy/main.go
+++ images/hooks/cmd/migrate-delete-renamed-validation-admission-policy/main.go
@@ -92,6 +92,11 @@ func reconcile(ctx context.Context, input *pkg.HookInput) error {
 	}
 	uts = append(uts, snapObjs...)
 
+	// TODO:
+	// use input.PatchCollector.Delete instead input.DC.GetK8sClient
+	//
+	// ddd,err := input.PatchCollector.Delete(apiVersion string, kind string, namespace string, name string)
+
 	c, err := input.DC.GetK8sClient()
 	if err != nil {
 		input.Logger.Error("Error get kubernetes client %v", err)
@@ -103,9 +108,11 @@ func reconcile(ctx context.Context, input *pkg.HookInput) error {
 			foundDeprecatedCount++
 			name := obj.GetName()
 			kind := obj.GetObjectKind().GroupVersionKind().Kind
+			apiVer := obj.GetAPIVersion()
 			input.Logger.Info("Delete deprecated %s %s", name, kind)
 
 			err = c.Delete(ctx, obj)
+			// err = input.PatchCollector.Delete(apiVer, kind, "", name)
 			if err != nil {
 				input.Logger.Error("%v, can't delete %s %s", err, name, kind)
 			}
