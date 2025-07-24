e7963c9614165d50b78640f227b4d022227a2550
Daniil Antoshin <daniil.antoshin@flant.com>
2025-07-24 15:37:58 +0200

    +
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- images/virtualization-artifact/pkg/controller/vdsnapshot/internal/life_cycle.go
+++ images/virtualization-artifact/pkg/controller/vdsnapshot/internal/life_cycle.go
@@ -249,7 +249,7 @@ func (h LifeCycleHandler) Handle(ctx context.Context, vdSnapshot *virtv2.Virtual
 			Spec: vsv1.VolumeSnapshotSpec{
 				Source: vsv1.VolumeSnapshotSource{
 					PersistentVolumeClaimName: &pvc.Name,
-					VolumeSnapshotContentName: ptr.To(""),
+					VolumeSnapshotContentName: ptr.To("default"),
 				},
 			},
 		}
