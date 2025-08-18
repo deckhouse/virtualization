74b7404cfba52c8ab4ab77c202b0eaa1777de710
Roman Sysoev <36233932+hardcoretime@users.noreply.github.com>
2025-08-18 12:35:55 +0300

    fix(vmsnapshot): watch for virtual disk phase and inUse condition (#1331)
    Signed-off-by: Roman Sysoev <roman.sysoev@flant.com>

--- images/virtualization-artifact/pkg/controller/vmsnapshot/internal/watcher/vd_watcher.go
+++ images/virtualization-artifact/pkg/controller/vmsnapshot/internal/watcher/vd_watcher.go
@@ -55,6 +55,17 @@ func (w VirtualDiskWatcher) Watch(mgr manager.Manager, ctr controller.Controller
 				CreateFunc: func(e event.TypedCreateEvent[*virtv2.VirtualDisk]) bool { return false },
 				DeleteFunc: func(e event.TypedDeleteEvent[*virtv2.VirtualDisk]) bool { return false },
 				UpdateFunc: func(e event.TypedUpdateEvent[*virtv2.VirtualDisk]) bool {
+					if e.ObjectOld.Status.Phase != e.ObjectNew.Status.Phase {
+						return true
+					}
+
+					oldInUseCondition, _ := conditions.GetCondition(vdcondition.InUseType, e.ObjectOld.Status.Conditions)
+					newInUseCondition, _ := conditions.GetCondition(vdcondition.InUseType, e.ObjectNew.Status.Conditions)
+
+					if oldInUseCondition != newInUseCondition {
+						return true
+					}
+
 					oldResized, _ := conditions.GetCondition(vdcondition.ResizingType, e.ObjectOld.Status.Conditions)
 					newResized, _ := conditions.GetCondition(vdcondition.ResizingType, e.ObjectNew.Status.Conditions)
 
