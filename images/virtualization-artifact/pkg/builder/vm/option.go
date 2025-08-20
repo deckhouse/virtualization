908b949da414820c5e24081fb3d01f5a09bcedb5
Yaroslav Borbat <yaroslav.borbat@flant.com>
2025-08-20 12:39:31 +0300

    add e2e framework
    Signed-off-by: Yaroslav Borbat <yaroslav.borbat@flant.com>

--- images/virtualization-artifact/pkg/builder/vm/option.go
+++ images/virtualization-artifact/pkg/builder/vm/option.go
@@ -56,3 +56,40 @@ func WithBlockDeviceRefs(bdRefs ...v1alpha2.BlockDeviceSpecRef) Option {
 		vm.Spec.BlockDeviceRefs = append(vm.Spec.BlockDeviceRefs, bdRefs...)
 	}
 }
+
+func WithDisks(disks ...*v1alpha2.VirtualDisk) Option {
+	return func(vm *v1alpha2.VirtualMachine) {
+		blockDeviceRefs := make([]v1alpha2.BlockDeviceSpecRef, 0, len(disks))
+		for _, disk := range disks {
+			blockDeviceRefs = append(blockDeviceRefs, v1alpha2.BlockDeviceSpecRef{
+				Kind: v1alpha2.VirtualDiskKind,
+				Name: disk.Name,
+			})
+		}
+		vm.Spec.BlockDeviceRefs = append(vm.Spec.BlockDeviceRefs, blockDeviceRefs...)
+	}
+}
+
+func WithBlockDeviceRefs(refs ...v1alpha2.BlockDeviceSpecRef) Option {
+	return func(vm *v1alpha2.VirtualMachine) {
+		vm.Spec.BlockDeviceRefs = append(vm.Spec.BlockDeviceRefs, refs...)
+	}
+}
+
+func WithNodeSelector(nodeSelector map[string]string) Option {
+	return func(vm *v1alpha2.VirtualMachine) {
+		vm.Spec.NodeSelector = nodeSelector
+	}
+}
+
+func WithLiveMigrationPolicy(liveMigrationPolicy v1alpha2.LiveMigrationPolicy) Option {
+	return func(vm *v1alpha2.VirtualMachine) {
+		vm.Spec.LiveMigrationPolicy = liveMigrationPolicy
+	}
+}
+
+func WithVirtualMachineClass(class string) Option {
+	return func(vm *v1alpha2.VirtualMachine) {
+		vm.Spec.VirtualMachineClassName = class
+	}
+}
