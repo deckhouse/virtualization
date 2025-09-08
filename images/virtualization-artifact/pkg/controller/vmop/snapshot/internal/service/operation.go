df14e496c5dd954753bdd1f34a4ef5a713ebe53e
Daniil Antoshin <daniil.antoshin@flant.com>
2025-09-08 19:48:15 +0200

    refactor lifecycle
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/service/operation.go
+++ images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/service/operation.go
@@ -34,7 +34,7 @@ type Operation interface {
 	IsApplicableForRunPolicy(runPolicy v1alpha2.RunPolicy) bool
 	GetInProgressReason() vmopcondition.ReasonCompleted
 	IsInProgress() bool
-	IsComplete() (bool, string)
+	IsComplete() bool
 }
 
 func NewOperationService(client client.Client, recorder eventrecord.EventRecorderLogger, vmop *v1alpha2.VirtualMachineOperation) (Operation, error) {
