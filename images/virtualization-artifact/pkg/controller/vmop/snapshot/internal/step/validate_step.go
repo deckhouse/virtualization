fb535eea350802bb42e4f1f485d5d981dbb53c6c
Daniil Antoshin <daniil.antoshin@flant.com>
2025-09-09 01:36:59 +0200

    fix types
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/step/validate_step.go
+++ images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/step/validate_step.go
@@ -29,7 +29,6 @@ import (
 	"github.com/deckhouse/virtualization-controller/pkg/common/object"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/conditions"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/service/restorer"
-	restorercommon "github.com/deckhouse/virtualization-controller/pkg/controller/service/restorer/common"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/vmop/snapshot/internal/common"
 	"github.com/deckhouse/virtualization-controller/pkg/eventrecord"
 	"github.com/deckhouse/virtualization/api/core/v1alpha2"
@@ -83,7 +82,7 @@ func (s ValidateStep) Take(ctx context.Context, vmop *v1alpha2.VirtualMachineOpe
 		return &reconcile.Result{}, err
 	}
 
-	snapshotResources := restorer.NewSnapshotResources(s.client, restorercommon.RestoreKind, vmop.Spec.Restore.Mode, restorerSecret, vmSnapshot, string(vmop.UID))
+	snapshotResources := restorer.NewSnapshotResources(s.client, v1alpha2.VMOPTypeRestore, vmop.Spec.Restore.Mode, restorerSecret, vmSnapshot, string(vmop.UID))
 
 	err = snapshotResources.Prepare(ctx)
 	if err != nil {
