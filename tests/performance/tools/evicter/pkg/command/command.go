d02eac4d031947e5dd9da60fbd44be330be3b6fe
Nikita Korolev <nikita.korolev@flant.com>
2025-09-25 00:54:42 +0300

    refactor cu
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- tests/performance/tools/evicter/pkg/command/command.go
+++ tests/performance/tools/evicter/pkg/command/command.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"evicter/helpers"
 	"evicter/internal"
@@ -13,28 +14,46 @@ import (
 var (
 	namespace string
 	target    int
+	duration  string
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "statistic",
-	Short: "get statistic for vm and vd in name space",
-	Long:  `large describe`,
+	Use:   "migrator",
+	Short: "continuously migrate a percentage of virtual machines",
+	Long:  `A tool that continuously migrates a specified percentage of virtual machines in a namespace`,
 	Args:  cobra.ArbitraryArgs,
-	Run:   startEvict,
+	Run:   startMigrator,
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&namespace, "ns", "n", "perf", "namespace to look for the VMs,VDs, default 'perf'")
-	rootCmd.Flags().IntVarP(&target, "target", "t", 10, "target percentage for VM evict")
+	rootCmd.Flags().StringVarP(&namespace, "ns", "n", "perf", "namespace to look for the VMs, default 'perf'")
+	rootCmd.Flags().IntVarP(&target, "target", "t", 10, "target percentage for VM migration (1-100)")
+	rootCmd.Flags().StringVarP(&duration, "duration", "d", "0", "duration to run the migrator (e.g., '30m', '1h', '0' for infinite). Default '0' for infinite")
 }
 
-func startEvict(cmd *cobra.Command, args []string) {
+func startMigrator(cmd *cobra.Command, args []string) {
+	// Validate target percentage
+	if target < 1 || target > 100 {
+		fmt.Println("Error: target percentage must be between 1 and 100")
+		os.Exit(1)
+	}
+
+	// Parse duration
+	var runDuration time.Duration
+	var err error
+	if duration != "0" {
+		runDuration, err = time.ParseDuration(duration)
+		if err != nil {
+			fmt.Printf("Error parsing duration '%s': %v\n", duration, err)
+			os.Exit(1)
+		}
+	}
+
 	// create client
 	client := helpers.CreateKubeConfig()
 
-	vms := internal.ShuffleVMs(client, namespace)
-	internal.VmopMigrateVMs(client, namespace, vms)
-	internal.MonMigr(client, namespace, vms)
+	// Start the continuous migrator
+	internal.StartContinuousMigrator(client, namespace, target, runDuration)
 }
 
 func Execute() {
