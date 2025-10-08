0d496bdf6130b7a78d6825c78b55518e1566e7d1
Maksim Fedotov <maksim.fedotov@flant.com>
2025-10-08 14:57:16 +0300

    fix libvirt pkgconfig
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- images/libvirt/install-libvirt.sh
+++ images/libvirt/install-libvirt.sh
@@ -541,8 +541,8 @@ $SRC_BUILD/tools/libvirt-guests.sh to /usr/libexec
 # $SRC_BUILD/tools/bash-completion/virsh to /usr/share/bash-completion/completions
 # $SRC_BUILD/tools/bash-completion/virt-admin to /usr/share/bash-completion/completions
 # $SRC_BUILD/tools/ssh-proxy/30-libvirt-ssh-proxy.conf to /etc/ssh/ssh_config.d
-# $SRC_BUILD/libvirt.pc to /usr/lib64/pkgconfig
-# $SRC_BUILD/libvirt-qemu.pc to /usr/lib64/pkgconfig
+$SRC_BUILD/libvirt.pc to /usr/lib64/pkgconfig
+$SRC_BUILD/libvirt-qemu.pc to /usr/lib64/pkgconfig
 # $SRC_BUILD/libvirt-lxc.pc to /usr/lib64/pkgconfig
 # $SRC_BUILD/libvirt-admin.pc to /usr/lib64/pkgconfig
 EOF
