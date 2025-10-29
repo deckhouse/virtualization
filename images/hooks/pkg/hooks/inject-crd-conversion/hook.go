76c399cdb9e1f2de3e90c8f7020b9918829ca14c
Daniil Loktev <lokt.daniil@gmail.com>
2025-10-29 11:55:59 +0300

    wip
    Signed-off-by: Daniil Loktev <lokt.daniil@gmail.com>

--- images/hooks/pkg/hooks/inject-crd-conversion/hook.go
+++ images/hooks/pkg/hooks/inject-crd-conversion/hook.go
@@ -25,7 +25,6 @@ import (
 
 	"github.com/deckhouse/module-sdk/pkg"
 	"github.com/deckhouse/module-sdk/pkg/registry"
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/types"
