520895f1677722d7085e6709aab1fcf5eef4d8c3
Ivan Mikheykin <ivan.mikheykin@flant.com>
2025-11-06 10:47:26 +0300

    chore(core): refactor cron source
    - Make cron source independent of the garbage collection manager.
    
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- images/virtualization-artifact/pkg/controller/gc/cron_source.go
+++ images/virtualization-artifact/pkg/controller/gc/cron_source.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/robfig/cron/v3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/utils/clock"
@@ -33,33 +34,41 @@ import (
 	"github.com/deckhouse/virtualization-controller/pkg/logger"
 )
 
+/**
+CronSource is an implementation of the controller-runtime Source interface.
+It periodically triggers and emit events for a list of objects.
+
+The component is independent of kubernetes client: developer should implement
+ObjectLister interface that CronSource will use to determine what to enqueue on trigger.
+
+NewSingleObjectLister can be used if the main objective is to get periodical event,
+but specific namespace and name are not important. Also, for this situation
+object name can be used to distinguish cron trigger from the kubernetes trigger.
+*/
+
 var _ source.Source = &CronSource{}
 
 const sourceName = "CronSource"
 
-type SourceGCManager interface {
-	ListForDelete(ctx context.Context, now time.Time) ([]client.Object, error)
-}
-
-func NewCronSource(scheduleSpec string, mgr SourceGCManager, log *log.Logger) (*CronSource, error) {
+func NewCronSource(scheduleSpec string, objLister ObjectLister, log *log.Logger) (*CronSource, error) {
 	schedule, err := cron.ParseStandard(scheduleSpec)
 	if err != nil {
 		return nil, fmt.Errorf("parsing standard spec %q: %w", scheduleSpec, err)
 	}
 
 	return &CronSource{
-		schedule: schedule,
-		mgr:      mgr,
-		log:      log.With("WatchSource", sourceName),
-		clock:    &clock.RealClock{},
+		schedule:  schedule,
+		objLister: objLister,
+		log:       log.With("WatchSource", sourceName),
+		clock:     &clock.RealClock{},
 	}, nil
 }
 
 type CronSource struct {
-	schedule cron.Schedule
-	mgr      SourceGCManager
-	log      *log.Logger
-	clock    clock.Clock
+	schedule  cron.Schedule
+	objLister ObjectLister
+	log       *log.Logger
+	clock     clock.Clock
 }
 
 func (c *CronSource) Start(ctx context.Context, queue workqueue.TypedRateLimitingInterface[reconcile.Request]) error {
@@ -70,7 +79,7 @@ func (c *CronSource) Start(ctx context.Context, queue workqueue.TypedRateLimitin
 			case <-ctx.Done():
 				return
 			case <-c.clock.After(nextTime):
-				c.addObjects(ctx, queue.Add)
+				c.enqueueObjects(ctx, queue.Add)
 				nextTime = nextScheduleTimeDuration(c.schedule, c.clock.Now())
 			}
 		}
@@ -78,20 +87,21 @@ func (c *CronSource) Start(ctx context.Context, queue workqueue.TypedRateLimitin
 	return nil
 }
 
-func (c *CronSource) addObjects(ctx context.Context, addToQueue func(reconcile.Request)) {
-	objs, err := c.mgr.ListForDelete(ctx, c.clock.Now())
+func (c *CronSource) enqueueObjects(ctx context.Context, queueAddFunc func(reconcile.Request)) {
+	now := c.clock.Now()
+	objs, err := c.objLister.List(ctx, now)
 	if err != nil {
 		c.log.Error("Failed to get ObjectList for delete", logger.SlogErr(err))
 		return
 	}
 
 	if len(objs) == 0 {
-		c.log.Debug("No resources, skip")
+		c.log.Debug(fmt.Sprintf("No resources at %s, skip queueing", now))
 		return
 	}
 
 	for _, obj := range objs {
-		addToQueue(reconcile.Request{
+		queueAddFunc(reconcile.Request{
 			NamespacedName: types.NamespacedName{
 				Namespace: obj.GetNamespace(),
 				Name:      obj.GetName(),
@@ -103,4 +113,34 @@ func (c *CronSource) addObjects(ctx context.Context, addToQueue func(reconcile.R
 
 func nextScheduleTimeDuration(schedule cron.Schedule, now time.Time) time.Duration {
 	return schedule.Next(now).Sub(now)
+}
+
+type ObjectLister interface {
+	List(ctx context.Context, now time.Time) ([]client.Object, error)
+}
+
+type ObjectListerImpl struct {
+	ListFunc func(ctx context.Context, now time.Time) ([]client.Object, error)
+}
+
+func (o *ObjectListerImpl) List(ctx context.Context, now time.Time) ([]client.Object, error) {
+	if o.ListFunc == nil {
+		return nil, nil
+	}
+	return o.ListFunc(ctx, now)
+}
+
+func NewObjectLister(listFunc func(ctx context.Context, now time.Time) ([]client.Object, error)) *ObjectListerImpl {
+	return &ObjectListerImpl{listFunc}
+}
+
+func NewSingleObjectLister(namespace, name string) *ObjectListerImpl {
+	return &ObjectListerImpl{ListFunc: func(ctx context.Context, now time.Time) ([]client.Object, error) {
+		return []client.Object{&metav1.PartialObjectMetadata{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespace,
+				Name:      name,
+			},
+		}}, nil
+	}}
 }
