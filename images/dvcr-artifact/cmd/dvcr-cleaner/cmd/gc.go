b5f01fa736809101e0f4c07445b29e86d81dfa3b
Ivan Mikheykin <ivan.mikheykin@flant.com>
2025-11-18 21:23:49 +0300

    feat(core): add auto-cleanup and check for dvcr-cleaner (#1675)
    * feat(core): add auto-cleanup and check for dvcr-cleaner
    
    Add new commands to support auto-cleanup:
    
    - dvcr-cleaner gc auto-cleanup — compares images in registry and in cluster, removes manifests not found in cluster, runs garbage collect.
    - dvcr-cleaner gc check — jst compares images in registry and in cluster and prints images eligible to cleanup.
    
    - Cleanup is disabled by default, set schedule explicitly in spec.settings.dvcr.gc.schedule to run cleanup periodically.
    
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- images/dvcr-artifact/cmd/dvcr-cleaner/cmd/gc.go
+++ images/dvcr-artifact/cmd/dvcr-cleaner/cmd/gc.go
@@ -17,13 +17,26 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"sort"
+	"strconv"
+	"time"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
+
+	"github.com/deckhouse/virtualization-controller/dvcr-importers/pkg/cleaner/kubernetes"
+	"github.com/deckhouse/virtualization-controller/dvcr-importers/pkg/cleaner/registry"
+	"github.com/deckhouse/virtualization-controller/dvcr-importers/pkg/cleaner/signal"
+	"github.com/deckhouse/virtualization-controller/dvcr-importers/pkg/cleaner/storage"
+	"github.com/deckhouse/virtualization-controller/dvcr-importers/pkg/humanize"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
 )
 
 var GcCmd = &cobra.Command{
@@ -51,8 +64,7 @@ var gcRunCmd = &cobra.Command{
 			return fmt.Errorf("cache data cannot be deleted: %w", err)
 		}
 
-		execCmd := exec.Command("registry", "garbage-collect", "/etc/docker/registry/config.yml", "--delete-untagged")
-		stdout, err := execCmd.Output()
+		stdout, err := registry.ExecGarbageCollect(context.Background())
 		if err != nil {
 			fmt.Println(err.Error())
 			return nil
@@ -63,6 +75,41 @@ var gcRunCmd = &cobra.Command{
 	},
 	SilenceUsage:  true,
 	SilenceErrors: true,
+}
+
+var (
+	MaintenanceSecretName string
+	GCTimeout             time.Duration
+	GCTimeoutDefault      = time.Minute * 10
+)
+
+var autoCleanupCmd = &cobra.Command{
+	Use:           "auto-cleanup [--maintenance-secret-name secret] [--gc-timeout duration]",
+	Short:         "`auto-cleanup` deletes all stale images that have no corresponding resource in the cluster and then runs garbage-collect to remove underlying blobs (Note: not to be run with kubectl exec until you 100% sure what are you doing)",
+	Args:          cobra.OnlyValidArgs,
+	RunE:          autoCleanupHandler,
+	SilenceUsage:  true,
+	SilenceErrors: true,
+}
+
+var checkCmd = &cobra.Command{
+	Use:           "check",
+	Short:         "`check` reports stale images that have no corresponding resource in the cluster",
+	Args:          cobra.OnlyValidArgs,
+	RunE:          checkCleanupHandler,
+	SilenceUsage:  true,
+	SilenceErrors: true,
+}
+
+func init() {
+	GcCmd.AddCommand(gcRunCmd)
+
+	// Add 'run' command.
+	GcCmd.AddCommand(autoCleanupCmd)
+	autoCleanupCmd.Flags().StringVar(&MaintenanceSecretName, "maintenance-secret-name", "", "update secret with result and annotation after the cleanup")
+	autoCleanupCmd.Flags().DurationVar(&GCTimeout, "gc-timeout", GCTimeoutDefault, "max time for running garbage collection command")
+	// Add 'check' command.
+	GcCmd.AddCommand(checkCmd)
 }
 
 func Confirm() (bool, error) {
@@ -82,6 +129,286 @@ func Confirm() (bool, error) {
 	return true, nil
 }
 
-func init() {
-	GcCmd.AddCommand(gcRunCmd)
+func autoCleanupHandler(cmd *cobra.Command, args []string) error {
+	fsInfoBeforeCleanup, err := registry.StorageStats()
+	if err != nil {
+		return fmt.Errorf("get repositories filesystem info before cleanup: %w", err)
+	}
+
+	var errs *multierror.Error
+
+	cleanupErr := performAutoCleanup()
+	if cleanupErr != nil {
+		errs = multierror.Append(errs, cleanupErr)
+	}
+
+	result := map[string]string{
+		"result": "success",
+	}
+	if cleanupErr != nil {
+		result["result"] = "fail"
+		result["error"] = cleanupErr.Error()
+	}
+
+	// Report disk usage.
+	fsInfoAfterCleanup, errFSInfo := registry.StorageStats()
+	if errFSInfo != nil {
+		errs = multierror.Append(errs, fmt.Errorf("get repositories filesystem info after cleanup: %w", errFSInfo))
+		fmt.Printf("Error! FS stats not available: %v\n", errFSInfo)
+	} else {
+		totalBytes := int64(fsInfoAfterCleanup.Total)
+		availableBytes := int64(fsInfoAfterCleanup.Available)
+		freedBytes := int64(fsInfoAfterCleanup.Available - fsInfoBeforeCleanup.Available)
+
+		result["total"] = strconv.FormatInt(totalBytes, 10)
+		result["available"] = strconv.FormatInt(availableBytes, 10)
+		result["freed"] = strconv.FormatInt(freedBytes, 10)
+		result["message"] = fmt.Sprintf("Freed space after cleanup: %s. Available space %s of total %s.",
+			humanize.Bytes(freedBytes),
+			humanize.Bytes(availableBytes),
+			humanize.Bytes(totalBytes),
+		)
+		// Also report to stdout.
+		fmt.Print(reportFSState(fsInfoAfterCleanup, &fsInfoBeforeCleanup))
+	}
+
+	// Terminate without waiting if no secret name was provided.
+	if MaintenanceSecretName == "" {
+		return errs.ErrorOrNil()
+	}
+
+	// Update maintenance secret and wait for termination signal.
+	secretErr := annotateMaintenanceSecretOnCleanupDone(context.Background(), result)
+	if secretErr != nil {
+		errs = multierror.Append(errs, secretErr)
+	}
+
+	// Return previous errors, so Pod will be restarted without waiting.
+	err = errs.ErrorOrNil()
+	if err != nil {
+		return err
+	}
+
+	// Wait until termination.
+	fmt.Println("Wait for signal before terminate.")
+	signal.WaitForTermination()
+	return nil
+}
+
+func performAutoCleanup() error {
+	absentImages, err := getAbsentImages()
+	if err != nil {
+		return err
+	}
+
+	// Delete manifests for absent images.
+	if len(absentImages) == 0 {
+		fmt.Println("No images eligible for cleanup.")
+	} else {
+		err = registry.RemoveImages(absentImages)
+		if err != nil {
+			return fmt.Errorf("remove manifests: %w", err)
+		}
+	}
+
+	// Run 'registry garbage-collect' to remove unused blobs.
+	gcContext, _ := context.WithTimeoutCause(context.Background(), GCTimeout, fmt.Errorf("garbage collect command is terminated, it runs more than %s", GCTimeout.String()))
+	stdout, err := registry.ExecGarbageCollect(gcContext)
+	errMsg := ""
+	if cause := context.Cause(gcContext); cause != nil {
+		errMsg = cause.Error() + "\n"
+	}
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			errMsg += fmt.Sprintf("Exit code: %d\nStderr: %s\n", exitErr.ExitCode(), exitErr.Stderr)
+		} else {
+			errMsg += err.Error()
+		}
+	}
+
+	if errMsg != "" {
+		return errors.New(errMsg)
+	}
+
+	fmt.Println(string(stdout))
+	return nil
+}
+
+func checkCleanupHandler(_ *cobra.Command, _ []string) error {
+	fsInfo, err := registry.StorageStats()
+	if err != nil {
+		return fmt.Errorf("get repositories filesystem info before cleanup: %w", err)
+	}
+
+	absentImages, err := getAbsentImages()
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(reportFSState(fsInfo, nil))
+
+	if len(absentImages) == 0 {
+		fmt.Println("No images eligible for auto-cleanup.")
+		return nil
+	}
+
+	sort.SliceStable(absentImages, func(i, j int) bool {
+		return absentImages[i].Path < absentImages[j].Path
+	})
+
+	fmt.Println("Images eligible for cleanup:")
+	fmt.Printf("%-22s %-20s %-45s\n", "KIND", "NAMESPACE", "NAME")
+	for _, image := range absentImages {
+		ns := image.Namespace
+		if image.Type == v1alpha2.ClusterVirtualImageKind {
+			ns = ""
+		}
+		fmt.Printf("%-22s %-20s %-45s\n", image.Type, ns, image.Name)
+	}
+
+	return nil
+}
+
+func getAbsentImages() ([]registry.Image, error) {
+	// List all images created for all ClusterVirtualImage and VirtualImage resources.
+	images, err := registry.ListImagesAll()
+	if err != nil {
+		return nil, fmt.Errorf("list all images: %w", err)
+	}
+
+	// Get all VirtualDisks, VirtualImages, and ClusterVirtualImages
+	virtClient, err := kubernetes.NewVirtualizationClient()
+	if err != nil {
+		return nil, fmt.Errorf("initialize Kubernetes client: %w", err)
+	}
+
+	kubeImages, err := virtClient.ListAllPossibleImages(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("list images in cluster: %w", err)
+	}
+
+	// Compare lists, return images absent in the cluster.
+	return compareRegistryAndClusterImages(images, kubeImages), nil
+}
+
+// compareRegistryAndClusterImages returns images that has no corresponding resource in the cluster.
+// VirtualDisks in Ready phase are considered for cleanup.
+func compareRegistryAndClusterImages(images []registry.Image, kubeImages []kubernetes.ImageInfo) []registry.Image {
+	// Create indexes for all resources found in cluster.
+	// A map for ClusterImages. Keys are names.
+	clusterVirtualImages := make(map[string]struct{})
+	// A map for virtualImages: namespace -> name
+	virtualImages := make(map[string]map[string]struct{})
+	// A map for virtualDisks: namespace -> name -> disk phase
+	virtualDisks := make(map[string]map[string]v1alpha2.DiskPhase)
+	for _, kubeImage := range kubeImages {
+		switch kubeImage.Type {
+		case v1alpha2.ClusterVirtualImageKind:
+			clusterVirtualImages[kubeImage.Name] = struct{}{}
+		case v1alpha2.VirtualImageKind:
+			if _, ok := virtualImages[kubeImage.Namespace]; !ok {
+				virtualImages[kubeImage.Namespace] = make(map[string]struct{})
+			}
+			virtualImages[kubeImage.Namespace][kubeImage.Name] = struct{}{}
+		case v1alpha2.VirtualDiskKind:
+			if _, ok := virtualDisks[kubeImage.Namespace]; !ok {
+				virtualDisks[kubeImage.Namespace] = make(map[string]v1alpha2.DiskPhase)
+			}
+			virtualDisks[kubeImage.Namespace][kubeImage.Name] = kubeImage.Phase
+		}
+	}
+
+	absentImages := make([]registry.Image, 0)
+	for _, image := range images {
+		switch image.Type {
+		case v1alpha2.ClusterVirtualImageKind:
+			if _, ok := clusterVirtualImages[image.Name]; !ok {
+				absentImages = append(absentImages, image)
+			}
+		case v1alpha2.VirtualImageKind:
+			if _, ok := virtualImages[image.Namespace]; !ok {
+				absentImages = append(absentImages, image)
+				continue
+			}
+			if _, ok := virtualImages[image.Namespace][image.Name]; !ok {
+				absentImages = append(absentImages, image)
+			}
+		case v1alpha2.VirtualDiskKind:
+			if _, ok := virtualDisks[image.Namespace]; !ok {
+				absentImages = append(absentImages, image)
+				continue
+			}
+			if _, ok := virtualDisks[image.Namespace][image.Name]; !ok {
+				absentImages = append(absentImages, image)
+				continue
+			}
+			// Images for disks in Ready phase are eligible for cleanup.
+			if virtualDisks[image.Namespace][image.Name] == v1alpha2.DiskReady {
+				absentImages = append(absentImages, image)
+			}
+		}
+	}
+
+	return absentImages
+}
+
+const (
+	garbageCollectionDoneAnno = "virtualization.deckhouse.io/dvcr-garbage-collection-done"
+	switchToMaintenanceAnno   = "virtualization.deckhouse.io/dvcr-deployment-switch-to-maintenance-mode"
+)
+
+func annotateMaintenanceSecretOnCleanupDone(ctx context.Context, result map[string]string) error {
+	resultBytes, err := json.Marshal(result)
+	if err != nil {
+		return fmt.Errorf("marshal result to json: %w", err)
+	}
+
+	// Get all VirtualImages and ClusterImages
+	virtClient, err := kubernetes.NewVirtualizationClient()
+	if err != nil {
+		return fmt.Errorf("initialize Kubernetes client: %w", err)
+	}
+
+	secret, err := virtClient.GetMaintenanceSecret(ctx)
+	if err != nil {
+		return err
+	}
+
+	if secret.Annotations == nil {
+		secret.Annotations = make(map[string]string)
+	}
+	secret.Annotations[garbageCollectionDoneAnno] = ""
+	delete(secret.Annotations, switchToMaintenanceAnno)
+
+	if secret.Data == nil {
+		secret.Data = make(map[string][]byte)
+	}
+	secret.Data["result"] = resultBytes
+
+	err = virtClient.UpdateMaintenanceSecret(ctx, secret)
+	if err != nil {
+		return fmt.Errorf("update secret on cleanup done: %w", err)
+	}
+
+	return nil
+}
+
+func reportFSState(fsInfo storage.FSInfo, fsInfoBefore *storage.FSInfo) (report string) {
+	total := humanize.Bytes(int64(fsInfo.Total))
+	used := humanize.Bytes(int64(fsInfo.Total - fsInfo.Available))
+	available := humanize.Bytes(int64(fsInfo.Available))
+	usePct := 100 - fsInfo.Available*100/fsInfo.Total
+	freed := ""
+	if fsInfoBefore != nil {
+		freedBytes := int64(fsInfo.Available - fsInfoBefore.Available)
+		freed = humanize.Bytes(freedBytes)
+	}
+
+	if fsInfoBefore != nil {
+		report += fmt.Sprintf("Freed space after cleanup: %s\n", freed)
+	}
+	report += fmt.Sprintf("%7s  %7s  %7s  %7s\n", "Total", "Used", "Avail", "Use%")
+	report += fmt.Sprintf("%7s  %7s  %7s  %6d%%\n", total, used, available, usePct)
+
+	return report
 }
---
+++ images/dvcr-artifact/pkg/cleaner/registry/list.go
@@ -0,0 +1,279 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package registry
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+)
+
+const (
+	RepoDir = "/var/lib/registry/docker/registry/v2/repositories"
+)
+
+type Image struct {
+	Type      string
+	Namespace string
+	Name      string
+	Path      string
+}
+
+var ErrVINonNamespacedImage = errors.New("VI repository without namespace")
+
+// clusterVirtualImagesDir returns a directory where stored all images for ClusterVirtualImage resources.
+//
+// Example:
+//
+//	 /.../repositories
+//	 `-- cvi
+//		    |-- ubuntu-24-04
+//		    `-- alpine-latest
+func clusterVirtualImagesDir() string {
+	return filepath.Join(RepoDir, "cvi")
+}
+
+// virtualImagesDir returns a directory where stored all images for VirtualImage resources.
+// These images are actually one level deep, there is a directory with the corresponding namespace name.
+//
+// Example:
+//
+//	  /.../repositories
+//	  `-- vi
+//		     |-- default
+//		     |   `-- ubuntu-24-04
+//		     |-- testvms
+//		         |-- alpine-latest
+//		         |-- win-server
+//		         `-- ubuntu-latest
+func virtualImagesDir() string {
+	return filepath.Join(RepoDir, "vi")
+}
+
+// virtualImagesDir returns a directory where stored all temporary images for VirtualDisk resources.
+// Directory structure is the same as for VirtualImage (see virtualImagesDir).
+func virtualDisksDir() string {
+	return filepath.Join(RepoDir, "vd")
+}
+
+// ListImagesAll return image info for all image types:
+// virtualImages, clusterVirtualImages and virtualDisks.
+func ListImagesAll() ([]Image, error) {
+	clusterVirtualImages, err := ListClusterVirtualImages()
+	if err != nil {
+		return nil, err
+	}
+
+	virtualImages, err := ListVirtualImagesAll()
+	if err != nil {
+		return nil, err
+	}
+
+	virtualDiskImages, err := ListVirtualDisksAll()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Found %d cvi, %d vi, %d vd manifests in registry\n",
+		len(clusterVirtualImages),
+		len(virtualImages),
+		len(virtualDiskImages),
+	)
+
+	clusterVirtualImages = append(clusterVirtualImages, virtualImages...)
+	return append(clusterVirtualImages, virtualDiskImages...), nil
+}
+
+func ListClusterVirtualImages() ([]Image, error) {
+	imagesDir := clusterVirtualImagesDir()
+
+	imagesEntries, err := os.ReadDir(imagesDir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("cannot get the list of all `ClusterVirtualImages`: %w", err)
+	}
+
+	images := make([]Image, 0)
+	for _, imageEntry := range imagesEntries {
+		images = append(images, Image{
+			Type:      v1alpha2.ClusterVirtualImageKind,
+			Namespace: "",
+			Name:      imageEntry.Name(),
+			Path:      filepath.Join(imagesDir, imageEntry.Name()),
+		})
+	}
+	return images, nil
+}
+
+func ListVirtualImagesAll() ([]Image, error) {
+	imagesDir := virtualImagesDir()
+
+	namespacesEntries, err := os.ReadDir(imagesDir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("cannot list directories for namespaces in %s: %w", imagesDir, err)
+	}
+
+	images := make([]Image, 0)
+	for _, nsEntry := range namespacesEntries {
+		nsImages, err := ListVirtualImagesForNamespace(nsEntry.Name())
+		if err != nil {
+			return nil, err
+		}
+		images = append(images, nsImages...)
+	}
+	return images, nil
+}
+
+func ListVirtualImagesForNamespace(namespace string) ([]Image, error) {
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	imagesDir := filepath.Join(virtualImagesDir(), namespace)
+	imagesEntries, err := os.ReadDir(imagesDir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("cannot list directories with images for `VirtualImage` resources in %s: %w", imagesDir, err)
+	}
+
+	images := make([]Image, 0)
+	for _, imageEntry := range imagesEntries {
+		if imageEntry.Name() == "_manifests" {
+			// Workaround for a bug: transient images for CVI with objectRef snapshot
+			// are uploaded into vi/<cvi-name> directories instead of cvi/<cvi-name>.
+			// There are no virtual images in directory passed as a "namespace" argument in this case:
+			// "namespace" directory is a manifest for ClusterVirtualImage.
+			// We add ClusterVirtualImage with name from "namespace" argument,
+			// no further iteration is required.
+			return []Image{{
+				Type: v1alpha2.ClusterVirtualImageKind,
+				Name: namespace,
+				Path: imagesDir,
+			}}, nil
+		}
+		images = append(images, Image{
+			Type:      v1alpha2.VirtualImageKind,
+			Namespace: namespace,
+			Name:      imageEntry.Name(),
+			Path:      filepath.Join(imagesDir, imageEntry.Name()),
+		})
+	}
+	return images, nil
+}
+
+func GetAnyImage(imageType, namespace, name string) (*Image, error) {
+	var (
+		imageDir string
+		fileInfo os.FileInfo
+		err      error
+	)
+
+	switch imageType {
+	case v1alpha2.VirtualImageKind:
+		if namespace == "" {
+			namespace = "default"
+		}
+		imageDir = virtualImagesDir()
+		path := filepath.Join(imageDir, namespace, name)
+		fileInfo, err = os.Stat(path)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				return nil, fmt.Errorf("the `%s` %q is not found in %q namespace", imageType, name, namespace)
+			}
+			return nil, fmt.Errorf("cannot get the `%s` %q in the %q namespace: %w", imageType, name, namespace, err)
+		}
+	case v1alpha2.ClusterVirtualImageKind:
+		imageDir = clusterVirtualImagesDir()
+		path := filepath.Join(imageDir, name)
+
+		fileInfo, err = os.Stat(path)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				return nil, fmt.Errorf("the `%s` %q is not found", imageType, name)
+			}
+			return nil, fmt.Errorf("cannot get the `%s` %q: %w", imageType, name, err)
+		}
+	default:
+		return nil, fmt.Errorf("unknown image type: %s", imageType)
+	}
+
+	return &Image{
+		Type:      imageType,
+		Namespace: namespace,
+		Name:      name,
+		Path:      filepath.Join(imageDir, fileInfo.Name()),
+	}, nil
+}
+
+func ListVirtualDisksAll() ([]Image, error) {
+	disksDir := virtualDisksDir()
+
+	namespacesEntries, err := os.ReadDir(disksDir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("cannot list directories for namespaces in %s: %w", disksDir, err)
+	}
+
+	disks := make([]Image, 0)
+	for _, nsEntry := range namespacesEntries {
+		disksInNamespace, err := ListVirtualDisksForNamespace(nsEntry.Name())
+		if err != nil {
+			return nil, err
+		}
+		disks = append(disks, disksInNamespace...)
+	}
+	return disks, nil
+}
+
+func ListVirtualDisksForNamespace(namespace string) ([]Image, error) {
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	disksDir := filepath.Join(virtualDisksDir(), namespace)
+	disksEntries, err := os.ReadDir(disksDir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("cannot list directories with images for `VirtualDisk` resources in %s: %w", disksDir, err)
+	}
+
+	disksImages := make([]Image, 0)
+	for _, diskEntry := range disksEntries {
+		disksImages = append(disksImages, Image{
+			Type:      v1alpha2.VirtualDiskKind,
+			Namespace: namespace,
+			Name:      diskEntry.Name(),
+			Path:      filepath.Join(disksDir, diskEntry.Name()),
+		})
+	}
+	return disksImages, nil
+}
