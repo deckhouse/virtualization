ce9972b0dbfe374ae11ba4ed2330c58d265094fa
Nikita Korolev <nikita.korolev@flant.com>
2025-12-02 09:51:58 +0300

    add dh queue check
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

---
+++ test/dvp-over-dvp/tools/deckhouse-queue.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+kubectl() {
+  sudo /opt/deckhouse/bin/kubectl $@
+}
+
+d8() {
+  sudo /opt/deckhouse/bin/d8 $@
+}
+
+
+d8_queue_main() {
+  echo "$( d8 p queue main | grep -Po '(?<=length )([0-9]+)' )"
+}
+
+d8_queue_list() {
+  d8 p queue list | grep -Po '([0-9]+)(?= active)'
+}
+
+d8_queue() {
+  count=20
+  local main_queue_ready=false
+  local list_queue_ready=false
+
+  for i in $(seq 1 $count) ; do
+    if [ $(d8_queue_main) == "0" ]; then
+      echo "main queue is clear"
+      main_queue_ready=true
+    else
+      d8 p queue main | head -n25
+    fi
+
+    if [ $(d8_queue_list) == "0" ]; then
+      echo "list queue is clear"
+      list_queue_ready=true
+    else
+      d8 p queue list | head -n25
+    fi
+
+    if [ "$main_queue_ready" = true ] && [ "$list_queue_ready" = true ]; then
+      break
+    fi
+    echo "Wait until queues are ready ${i}/${count}"
+    sleep 5
+  done
+}
+
+d8_ready() {
+  local ready=false
+  local count=10
+  for i in $(seq 1 $count) ; do
+    echo "Wait until deckhouse is ready ${i}/${count}"
+    if kubectl -n d8-system wait deploy/deckhouse --for condition=available --timeout=60s; then
+      ready=true
+      break
+    fi
+  done
+
+  if [ "$ready" = true ]; then
+    success_echo "Deckhouse is Ready!"
+    echo "Checking queues"
+    d8_queue
+  else
+    error_echo "Deckhouse is not ready after ${count}m, check its queue for errors:"
+    d8 p queue main | head -n25
+    exit 1
+  fi
+}
+
+start_time=$(date +%s)
+echo "Checking that deckhouse is ready"
+d8_ready
+end_time=$(date +%s)
+difference=$((end_time - start_time))
+echo "Deckhouse is ready after $(date -ud "@$difference" +'%H:%M:%S')"
