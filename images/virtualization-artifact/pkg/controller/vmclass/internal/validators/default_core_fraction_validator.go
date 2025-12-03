a958594ab8c951290b285190cdb65f6d93f6598b
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2025-12-03 22:42:29 +0300

    t
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

---
+++ images/virtualization-artifact/pkg/controller/vmclass/internal/validators/default_core_fraction_validator.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validators
+
+import (
+	"context"
+	"fmt"
+
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+)
+
+type DefaultCoreFractionValidator struct{}
+
+func NewDefaultCoreFractionValidator() *DefaultCoreFractionValidator {
+	return &DefaultCoreFractionValidator{}
+}
+
+func (v *DefaultCoreFractionValidator) ValidateCreate(_ context.Context, vmclass *v1alpha2.VirtualMachineClass) (admission.Warnings, error) {
+	return nil, v.validate(vmclass)
+}
+
+func (v *DefaultCoreFractionValidator) ValidateUpdate(_ context.Context, _, newVMClass *v1alpha2.VirtualMachineClass) (admission.Warnings, error) {
+	return nil, v.validate(newVMClass)
+}
+
+func (v *DefaultCoreFractionValidator) validate(vmclass *v1alpha2.VirtualMachineClass) error {
+	for i, policy := range vmclass.Spec.SizingPolicies {
+		if policy.DefaultCoreFraction == nil {
+			continue
+		}
+
+		if len(policy.CoreFractions) == 0 {
+			continue
+		}
+
+		if !containsCoreFraction(policy.CoreFractions, *policy.DefaultCoreFraction) {
+			return fmt.Errorf("vmclass %s sizingPolicy[%d]: defaultCoreFraction %d is not in the allowed coreFractions list %v",
+				vmclass.Name, i, *policy.DefaultCoreFraction, policy.CoreFractions)
+		}
+	}
+
+	return nil
+}
+
+func containsCoreFraction(fractions []v1alpha2.CoreFractionValue, value v1alpha2.CoreFractionValue) bool {
+	for _, f := range fractions {
+		if f == value {
+			return true
+		}
+	}
+	return false
+}
+
