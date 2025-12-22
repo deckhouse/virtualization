186be9b50c6486635d5273a6380c793d35354f52
Maksim Fedotov <maksim.fedotov@flant.com>
2025-12-22 12:33:13 +0300

    chore(test): change images url
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- test/e2e/internal/object/const.go
+++ test/e2e/internal/object/const.go
@@ -17,9 +17,9 @@ limitations under the License.
 package object
 
 const (
-	ImageURLAlpineUEFIPerf = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/alpine/alpine-3-21-uefi-perf.qcow2"
-	ImageURLUbuntu         = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/ubuntu/ubuntu-24.04-minimal-cloudimg-amd64.qcow2"
-	ImageURLAlpineBIOS     = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/alpine/alpine-3-21-bios-base.qcow2"
+	ImageURLAlpineUEFIPerf = "https://e2e-test-images.e2e.virtlab.flant.com/alpine-3-21-uefi-perf.qcow2"
+	ImageURLUbuntu         = "https://e2e-test-images.e2e.virtlab.flant.com/ubuntu-24.04-minimal-cloudimg-amd64.qcow2"
+	ImageURLAlpineBIOS     = "https://e2e-test-images.e2e.virtlab.flant.com/alpine-3-21-bios-base.qcow2"
 	ImageURLContainerImage = "cr.yandex/crpvs5j3nh1mi2tpithr/e2e/alpine/alpine-image:latest"
 	Mi256                  = 256 * 1024 * 1024
 	DefaultVMClass         = "generic"
