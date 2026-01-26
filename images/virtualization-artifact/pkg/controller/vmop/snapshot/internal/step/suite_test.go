bbfa6451ce36772fa5d6f0db3bacfbe384cd97b0
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2026-01-26 18:37:47 +0300

    restore
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    refactoring
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    fix
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    remove printf
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    fix e2e
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    total rewriting
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    fixes
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    tests
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    fix linter
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    fix linter 2
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    final -> completed
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
    
    resolve
    
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

---
+++ images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/step/suite_test.go
@@ -0,0 +1,157 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package step
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/deckhouse/virtualization-controller/pkg/common/annotations"
+	"github.com/deckhouse/virtualization-controller/pkg/eventrecord"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2/vmopcondition"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2/vmscondition"
+)
+
+func TestSteps(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "VMOP Snapshot Steps Suite")
+}
+
+func newNoOpRecorder() *eventrecord.EventRecorderLoggerMock {
+	return &eventrecord.EventRecorderLoggerMock{
+		EventFunc:           func(_ client.Object, _, _, _ string) {},
+		EventfFunc:          func(_ client.Object, _, _, _ string, _ ...interface{}) {},
+		AnnotatedEventfFunc: func(_ client.Object, _ map[string]string, _, _, _ string, _ ...interface{}) {},
+	}
+}
+
+//nolint:unparam // namespace is always "default" in tests, but kept for flexibility
+func createVMSnapshot(namespace, name, secretName string, ready bool) *v1alpha2.VirtualMachineSnapshot {
+	vms := &v1alpha2.VirtualMachineSnapshot{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1alpha2.VirtualMachineSnapshotKind,
+			APIVersion: v1alpha2.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha2.VirtualMachineSnapshotSpec{
+			VirtualMachineName: "test-vm",
+		},
+		Status: v1alpha2.VirtualMachineSnapshotStatus{
+			Phase:                            v1alpha2.VirtualMachineSnapshotPhaseReady,
+			VirtualMachineSnapshotSecretName: secretName,
+		},
+	}
+
+	if ready {
+		vms.Status.Conditions = []metav1.Condition{
+			{
+				Type:   string(vmscondition.VirtualMachineSnapshotReadyType),
+				Status: metav1.ConditionTrue,
+			},
+		}
+	}
+
+	return vms
+}
+
+//nolint:unparam // namespace is always "default" in tests, but kept for flexibility
+func createRestoreVMOP(namespace, name, vmName, snapshotName string) *v1alpha2.VirtualMachineOperation {
+	return &v1alpha2.VirtualMachineOperation{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1alpha2.VirtualMachineOperationKind,
+			APIVersion: v1alpha2.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       types.UID("test-vmop-uid"),
+		},
+		Spec: v1alpha2.VirtualMachineOperationSpec{
+			Type:           v1alpha2.VMOPTypeRestore,
+			VirtualMachine: vmName,
+			Restore: &v1alpha2.VirtualMachineOperationRestoreSpec{
+				Mode:                       v1alpha2.SnapshotOperationModeStrict,
+				VirtualMachineSnapshotName: snapshotName,
+			},
+		},
+	}
+}
+
+//nolint:unparam // namespace is always "default" in tests, but kept for flexibility
+func createCloneVMOP(namespace, name, vmName, snapshotName string) *v1alpha2.VirtualMachineOperation {
+	vmop := &v1alpha2.VirtualMachineOperation{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1alpha2.VirtualMachineOperationKind,
+			APIVersion: v1alpha2.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       types.UID("test-vmop-uid"),
+			Annotations: map[string]string{
+				annotations.AnnVMOPSnapshotName: snapshotName,
+			},
+		},
+		Spec: v1alpha2.VirtualMachineOperationSpec{
+			Type:           v1alpha2.VMOPTypeClone,
+			VirtualMachine: vmName,
+			Clone: &v1alpha2.VirtualMachineOperationCloneSpec{
+				Mode: v1alpha2.SnapshotOperationModeStrict,
+				Customization: &v1alpha2.VirtualMachineOperationCloneCustomization{
+					NameSuffix: "-clone",
+				},
+			},
+		},
+	}
+	return vmop
+}
+
+func setMaintenanceCondition(vmop *v1alpha2.VirtualMachineOperation, status metav1.ConditionStatus) {
+	vmop.Status.Conditions = append(vmop.Status.Conditions, metav1.Condition{
+		Type:   string(vmopcondition.TypeMaintenanceMode),
+		Status: status,
+	})
+}
+
+//nolint:unparam // namespace is always "default" in tests, but kept for flexibility
+func createVirtualDisk(namespace, name, ownerUID string, phase v1alpha2.DiskPhase) *v1alpha2.VirtualDisk {
+	return &v1alpha2.VirtualDisk{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "VirtualDisk",
+			APIVersion: v1alpha2.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Annotations: map[string]string{
+				annotations.AnnVMOPRestore: ownerUID,
+			},
+		},
+		Status: v1alpha2.VirtualDiskStatus{
+			Phase: phase,
+		},
+	}
+}
