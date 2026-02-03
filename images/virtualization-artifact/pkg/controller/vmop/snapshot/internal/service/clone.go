c40c1dfa9628fd0c743bde35561752d6a0f1696b
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2026-02-03 18:34:02 +0300

    resolve
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/service/clone.go
+++ images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/service/clone.go
@@ -29,6 +29,7 @@ import (
 	"github.com/deckhouse/virtualization-controller/pkg/common/object"
 	"github.com/deckhouse/virtualization-controller/pkg/common/steptaker"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/conditions"
+	"github.com/deckhouse/virtualization-controller/pkg/controller/service"
 	"github.com/deckhouse/virtualization-controller/pkg/controller/vmop/snapshot/internal/step"
 	"github.com/deckhouse/virtualization-controller/pkg/eventrecord"
 	"github.com/deckhouse/virtualization-controller/pkg/logger"
@@ -51,10 +52,20 @@ type CloneOperation struct {
 }
 
 func (o CloneOperation) Execute(ctx context.Context) (reconcile.Result, error) {
+	cb := conditions.NewConditionBuilder(vmopcondition.TypeCompleted)
+
+	defer func() {
+		if cb.Condition().Reason == string(vmopcondition.ReasonOperationFailed) {
+			o.vmop.Status.Phase = v1alpha2.VMOPPhaseFailed
+			conditions.SetCondition(cb, &o.vmop.Status.Conditions)
+		}
+	}()
+
 	log := logger.FromContext(ctx)
 
 	if o.vmop.Spec.Clone == nil {
 		err := fmt.Errorf("clone specification is mandatory to start cloning")
+		cb.Status(metav1.ConditionFalse).Reason(vmopcondition.ReasonOperationFailed).Message(service.CapitalizeFirstLetter(err.Error()))
 		return reconcile.Result{}, err
 	}
 
@@ -62,11 +73,13 @@ func (o CloneOperation) Execute(ctx context.Context) (reconcile.Result, error) {
 	vm, err := object.FetchObject(ctx, vmKey, o.client, &v1alpha2.VirtualMachine{})
 	if err != nil {
 		err := fmt.Errorf("failed to fetch the virtual machine %q: %w", vmKey.Name, err)
+		cb.Status(metav1.ConditionFalse).Reason(vmopcondition.ReasonOperationFailed).Message(service.CapitalizeFirstLetter(err.Error()))
 		return reconcile.Result{}, err
 	}
 
 	if vm == nil {
 		err := fmt.Errorf("specified virtual machine was not found")
+		cb.Status(metav1.ConditionFalse).Reason(vmopcondition.ReasonOperationFailed).Message(service.CapitalizeFirstLetter(err.Error()))
 		return reconcile.Result{}, err
 	}
 
@@ -85,14 +98,7 @@ func (o CloneOperation) Execute(ctx context.Context) (reconcile.Result, error) {
 		log.Debug(failMsg, logger.SlogErr(err))
 		failMsg = fmt.Errorf("%s: %w", failMsg, err).Error()
 		o.recorder.Event(o.vmop, corev1.EventTypeWarning, v1alpha2.ReasonErrVMOPFailed, failMsg)
-
-		o.vmop.Status.Phase = v1alpha2.VMOPPhaseFailed
-		conditions.SetCondition(
-			conditions.NewConditionBuilder(vmopcondition.TypeCompleted).
-				Reason(vmopcondition.ReasonOperationFailed).
-				Message(failMsg).Status(metav1.ConditionFalse),
-			&o.vmop.Status.Conditions,
-		)
+		cb.Reason(vmopcondition.ReasonOperationFailed).Message(failMsg).Status(metav1.ConditionFalse)
 	}
 
 	return result, err
