a6d3ca06f46a0907430230cbe66a6a089764e905
Dmitry Prytkov <dmitry.prytkov@flant.com>
2026-02-17 19:01:08 +0300

    test(vm): Check that the tpm module is recognized in the os
    Use Eventually
    
    Signed-off-by: Dmitry Prytkov <dmitry.prytkov@flant.com>

--- test/e2e/vm/tpm.go
+++ test/e2e/vm/tpm.go
@@ -24,13 +24,11 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/utils/ptr"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/deckhouse/virtualization-controller/pkg/builder/vd"
 	"github.com/deckhouse/virtualization-controller/pkg/builder/vm"
 	"github.com/deckhouse/virtualization/api/core/v1alpha2"
 	"github.com/deckhouse/virtualization/test/e2e/internal/framework"
-	"github.com/deckhouse/virtualization/test/e2e/internal/util"
 )
 
 var _ = Describe("VMCheckTPM", func() {
@@ -46,8 +44,8 @@ var _ = Describe("VMCheckTPM", func() {
 		By("Create a VM with the TPM module.")
 		const (
 			expectedTPMVersion = "2.0"
-			imageURLDebian12   = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/debian/debian-12-generic-amd64-20250814-2204.qcow2"
-			vdsize             = "4Gi"
+			imageURLDebian12   = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/debian/debian-12-with-tpm2-tools-amd64-20250814-2204.qcow2"
+			vdsize             = "4.4Gi"
 			bootLoader         = "EFI"
 			osType             = "Windows"
 			cloudInit          = `#cloud-config
@@ -61,9 +59,6 @@ users:
     lock_passwd: False
     ssh_authorized_keys:
       - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxcXHmwaGnJ8scJaEN5RzklBPZpVSic4GdaAsKjQoeA your_email@example.com
-packages:
-  - qemu-guest-agent
-  - tpm2-tools
 runcmd:
   - systemctl enable qemu-guest-agent --now
   - chown -R cloud:cloud /home/cloud
@@ -92,14 +87,19 @@ runcmd:
 		err := f.CreateWithDeferredDeletion(context.Background(), vdRoot, vmTPM)
 		Expect(err).NotTo(HaveOccurred())
 
-		By("Waits QEMU agent to be ready")
-		util.UntilVMAgentReady(client.ObjectKeyFromObject(vmTPM), framework.LongTimeout)
-
 		By(fmt.Sprintf("Checks that the VM has the TPM module version %s.", expectedTPMVersion))
+
 		cmd := `sudo tpm2_getcap properties-fixed | grep -A2 TPM2_PT_FAMILY_INDICATOR | grep value`
 
-		cmdStdOut, err := f.SSHCommand(vmTPM.Name, vmTPM.Namespace, cmd)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(cmdStdOut).To(ContainSubstring(expectedTPMVersion))
+		Eventually(func() error {
+			cmdStdOut, err := f.SSHCommand(vmTPM.Name, vmTPM.Namespace, cmd)
+			if err != nil {
+				return err
+			}
+
+			Expect(cmdStdOut).To(ContainSubstring(expectedTPMVersion))
+
+			return nil
+		}).WithTimeout(framework.LongTimeout).WithPolling(framework.PollingInterval).Should(Succeed())
 	})
 })
