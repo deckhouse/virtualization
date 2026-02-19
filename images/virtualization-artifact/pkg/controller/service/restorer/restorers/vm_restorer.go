fecedfd6a094fae146328b45ab8cf5500679ff3c
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2026-02-19 16:19:53 +0300

    fix linter
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- images/virtualization-artifact/pkg/controller/service/restorer/restorers/vm_restorer.go
+++ images/virtualization-artifact/pkg/controller/service/restorer/restorers/vm_restorer.go
@@ -250,7 +250,7 @@ func (v *VirtualMachineHandler) ProcessClone(ctx context.Context) error {
 		return err
 	}
 
-    // Do not clone labels due to potential issues with traffic from services.
+	// Do not clone labels due to potential issues with traffic from services.
 	v.vm.Labels = map[string]string{}
 
 	err = v.client.Create(ctx, v.vm)
