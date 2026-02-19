590c2a64817718ee90b8a77d2551b43133548da6
Valeriy Khorunzhin <b1kapb999@gmail.com>
2026-02-19 14:37:07 +0300

    chore(vmop): refactoring vmop restore/clone (#1925)
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

---
+++ images/virtualization-artifact/pkg/controller/vmop/snapshot/internal/step/completed_step.go
@@ -0,0 +1,50 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package step
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/deckhouse/virtualization-controller/pkg/controller/conditions"
+	"github.com/deckhouse/virtualization-controller/pkg/eventrecord"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2/vmopcondition"
+)
+
+type CompletedStep struct {
+	recorder eventrecord.EventRecorderLogger
+}
+
+func NewCompletedStep(
+	recorder eventrecord.EventRecorderLogger,
+) *CompletedStep {
+	return &CompletedStep{
+		recorder: recorder,
+	}
+}
+
+func (s CompletedStep) Take(ctx context.Context, vmop *v1alpha2.VirtualMachineOperation) (*reconcile.Result, error) {
+	cb := conditions.NewConditionBuilder(vmopcondition.TypeCompleted).Status(metav1.ConditionTrue).Reason(vmopcondition.ReasonOperationCompleted)
+	conditions.SetCondition(cb, &vmop.Status.Conditions)
+	vmop.Status.Phase = v1alpha2.VMOPPhaseCompleted
+	s.recorder.Event(vmop, corev1.EventTypeNormal, v1alpha2.ReasonVMOPSucceeded, "VirtualMachineOperation is successfully completed")
+	return &reconcile.Result{}, nil
+}
