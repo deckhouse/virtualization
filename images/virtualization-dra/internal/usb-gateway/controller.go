c26cd1d849d2a74271812fef7d0515eb70bd3f1c
Yaroslav Borbat <yaroslav.borbat@flant.com>
2026-02-27 18:08:40 +0300

    fix
    Signed-off-by: Yaroslav Borbat <yaroslav.borbat@flant.com>

--- images/virtualization-dra/internal/usb-gateway/controller.go
+++ images/virtualization-dra/internal/usb-gateway/controller.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package usbgateway
 
 import (
@@ -52,7 +68,6 @@ func NewUSBGatewayController(
 	nodeInformer, resourceSliceInformer cache.SharedIndexInformer,
 	usbIP usbip.Interface,
 ) (*USBGatewayController, error) {
-
 	queue := workqueue.NewTypedRateLimitingQueueWithConfig(
 		workqueue.DefaultTypedControllerRateLimiter[string](),
 		workqueue.TypedRateLimitingQueueConfig[string]{Name: controllerName},
@@ -206,7 +221,7 @@ func (c *USBGatewayController) Sync(ctx context.Context, key string) error {
 		return err
 	}
 
-	node, err = c.ensureAttachInfo(ctx, node)
+	_, err = c.ensureAttachInfo(ctx, node)
 	if err != nil {
 		return err
 	}
@@ -244,7 +259,6 @@ func (c *USBGatewayController) syncAddresses(node *corev1.Node, key string) {
 	if !exists {
 		c.removeAddr(node.Name)
 		return
-
 	}
 
 	err := validateUSBIPAddress(addr)
