cfa0376812aa8b1ece57d46ea6f312289f2f5ce3
Nikita Korolev <nikita.korolev@flant.com>
2026-03-06 13:55:57 +0300

    test: fix LocalVirtualDiskMigration
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- test/e2e/vm/volume_migration_local_disks.go
+++ test/e2e/vm/volume_migration_local_disks.go
@@ -70,7 +70,7 @@ var _ = Describe("LocalVirtualDiskMigration", decoratorsForVolumeMigrations(), f
 
 		DeferCleanup(f.After)
 
-		newVI := object.NewGeneratedHTTPVIAlpineBIOS("volume-migration-local-disks-", f.Namespace().Name)
+		newVI := object.NewGeneratedHTTPVIAlpineBIOSPerf("volume-migration-local-disks-", f.Namespace().Name)
 		newVI, err := f.VirtClient().VirtualImages(f.Namespace().Name).Create(context.Background(), newVI, metav1.CreateOptions{})
 		Expect(err).NotTo(HaveOccurred())
 		f.DeferDelete(newVI)
