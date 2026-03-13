6b4371c1fc4f3b8df00edc330c9219f97b3a4120
Nikita Korolev <nikita.korolev@flant.com>
2026-03-13 13:15:30 +0300

    refactor complex test
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- test/e2e/internal/object/const.go
+++ test/e2e/internal/object/const.go
@@ -23,6 +23,7 @@ const (
 	ImagesURLAlpineBIOSPerf = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/alpine/alpine-3-21-bios-perf.qcow2"
 	ImageURLUbuntu          = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/ubuntu/ubuntu-24.04-minimal-cloudimg-amd64.qcow2"
 	ImageURLContainerImage  = "cr.yandex/crpvs5j3nh1mi2tpithr/e2e/alpine/alpine-image:latest"
+	ImageURLLegacyContainerImage = "cr.yandex/crpvs5j3nh1mi2tpithr/e2e/alpine/alpine-3-20:latest"
 	// No bootable
 	ImageTestDataQCOW = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/test/test.qcow2"
 	ImageTestDataISO  = "https://89d64382-20df-4581-8cc7-80df331f67fa.selstorage.ru/test/test.iso"
