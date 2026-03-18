8ff76db47910b12301e09bf571872b6f4d1f718e
Maksim Fedotov <maksim.fedotov@flant.com>
2026-03-18 18:53:53 +0300

    comment out vmsop
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- test/e2e/snapshot/vmsop.go
+++ test/e2e/snapshot/vmsop.go
@@ -51,6 +51,11 @@ var _ = Describe("VMSOPCreateVirtualMachine", Ordered, func() {
 	)
 
 	BeforeAll(func() {
+		// cfg := framework.GetConfig()
+		// if cfg.StorageClass.TemplateStorageClass != nil && cfg.StorageClass.TemplateStorageClass.Provisioner == "nfs.csi.k8s.io" {
+		// 	Skip("Not working on NFS, skipping")
+		// }
+
 		DeferCleanup(f.After)
 
 		f.Before()
