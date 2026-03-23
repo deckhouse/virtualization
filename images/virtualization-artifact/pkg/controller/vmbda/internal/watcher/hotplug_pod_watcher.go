5d71be25dcabcf79f47266dc6e2e6f299adba0c5
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2026-03-24 02:08:51 +0300

    dirty
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

--- images/virtualization-artifact/pkg/controller/vmbda/internal/watcher/hotplug_pod_watcher.go
+++ images/virtualization-artifact/pkg/controller/vmbda/internal/watcher/hotplug_pod_watcher.go
@@ -19,6 +19,7 @@ package watcher
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -36,57 +37,45 @@ import (
 	"github.com/deckhouse/virtualization/api/core/v1alpha2"
 )
 
-const (
-	ReasonFailedAttachVolume = "FailedAttachVolume"
-	ReasonFailedMount        = "FailedMount"
-)
-
-func NewVolumeEventWatcher(client client.Client) *VolumeEventWatcher {
-	return &VolumeEventWatcher{
+func NewHotPlugPodWatcher(client client.Client) *HotPlugPodWatcher {
+	return &HotPlugPodWatcher{
 		client: client,
 	}
 }
 
-type VolumeEventWatcher struct {
+type HotPlugPodWatcher struct {
 	client client.Client
 }
 
-func (w *VolumeEventWatcher) Watch(mgr manager.Manager, ctr controller.Controller) error {
+func (w *HotPlugPodWatcher) Watch(mgr manager.Manager, ctr controller.Controller) error {
 	if err := ctr.Watch(
 		source.Kind(
 			mgr.GetCache(),
-			&corev1.Event{},
+			&corev1.Pod{},
 			handler.TypedEnqueueRequestsFromMapFunc(w.enqueueVMBDAs),
-			predicate.TypedFuncs[*corev1.Event]{
-				CreateFunc: func(e event.TypedCreateEvent[*corev1.Event]) bool {
-					return e.Object.Type == corev1.EventTypeWarning &&
-						(e.Object.Reason == ReasonFailedAttachVolume || e.Object.Reason == ReasonFailedMount)
-				},
-				UpdateFunc: func(e event.TypedUpdateEvent[*corev1.Event]) bool {
-					return false
-				},
-				DeleteFunc: func(e event.TypedDeleteEvent[*corev1.Event]) bool {
-					return false
+			predicate.TypedFuncs[*corev1.Pod]{
+				CreateFunc: func(e event.TypedCreateEvent[*corev1.Pod]) bool { return true },
+				DeleteFunc: func(e event.TypedDeleteEvent[*corev1.Pod]) bool { return true },
+				UpdateFunc: func(e event.TypedUpdateEvent[*corev1.Pod]) bool {
+					return e.ObjectOld.Status.Phase != e.ObjectNew.Status.Phase ||
+						!reflect.DeepEqual(e.ObjectOld.Status.Conditions, e.ObjectNew.Status.Conditions) ||
+						!reflect.DeepEqual(e.ObjectOld.Status.ContainerStatuses, e.ObjectNew.Status.ContainerStatuses)
 				},
 			},
 		),
 	); err != nil {
-		return fmt.Errorf("error setting watch on Event: %w", err)
+		return fmt.Errorf("error setting watch on hot-plug Pod: %w", err)
 	}
 	return nil
 }
 
-func (w *VolumeEventWatcher) enqueueVMBDAs(ctx context.Context, e *corev1.Event) []reconcile.Request {
-	if e.InvolvedObject.Kind != "Pod" {
+func (w *HotPlugPodWatcher) enqueueVMBDAs(ctx context.Context, pod *corev1.Pod) []reconcile.Request {
+	if pod == nil {
 		return nil
 	}
 
-	if e.Reason != ReasonFailedAttachVolume && e.Reason != ReasonFailedMount {
-		return nil
-	}
-
-	ns := e.InvolvedObject.Namespace
-	podName := e.InvolvedObject.Name
+	ns := pod.Namespace
+	podName := pod.Name
 
 	var kvvmiList virtv1.VirtualMachineInstanceList
 	if err := w.client.List(ctx, &kvvmiList, &client.ListOptions{Namespace: ns}); err != nil {
