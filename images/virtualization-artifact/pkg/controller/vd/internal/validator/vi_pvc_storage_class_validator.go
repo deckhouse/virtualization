5994cee1ed05949cadb711ac37d7dcc0d1c76fac
Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>
2026-04-22 11:57:10 +0300

    add webhook
    Signed-off-by: Valeriy Khorunzhin <valeriy.khorunzhin@flant.com>

---
+++ images/virtualization-artifact/pkg/controller/vd/internal/validator/vi_pvc_storage_class_validator.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validator
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/deckhouse/virtualization-controller/pkg/common/object"
+	"github.com/deckhouse/virtualization-controller/pkg/controller/conditions"
+	"github.com/deckhouse/virtualization-controller/pkg/controller/service"
+	intsvc "github.com/deckhouse/virtualization-controller/pkg/controller/vd/internal/service"
+	"github.com/deckhouse/virtualization-controller/pkg/controller/vd/internal/source"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2/vdcondition"
+)
+
+type VirtualImagePVCStorageClassValidator struct {
+	client    client.Client
+	scService *intsvc.VirtualDiskStorageClassService
+}
+
+func NewVirtualImagePVCStorageClassValidator(client client.Client, scService *intsvc.VirtualDiskStorageClassService) *VirtualImagePVCStorageClassValidator {
+	return &VirtualImagePVCStorageClassValidator{
+		client:    client,
+		scService: scService,
+	}
+}
+
+func (v *VirtualImagePVCStorageClassValidator) ValidateCreate(ctx context.Context, vd *v1alpha2.VirtualDisk) (admission.Warnings, error) {
+	return nil, v.validate(ctx, vd)
+}
+
+func (v *VirtualImagePVCStorageClassValidator) ValidateUpdate(ctx context.Context, _, newVD *v1alpha2.VirtualDisk) (admission.Warnings, error) {
+	ready, _ := conditions.GetCondition(vdcondition.ReadyType, newVD.Status.Conditions)
+	if source.IsDiskProvisioningFinished(ready) {
+		return nil, nil
+	}
+
+	return nil, v.validate(ctx, newVD)
+}
+
+func (v *VirtualImagePVCStorageClassValidator) validate(ctx context.Context, vd *v1alpha2.VirtualDisk) error {
+	if vd.Spec.DataSource == nil {
+		return nil
+	}
+
+	isObjectRef := vd.Spec.DataSource.Type == v1alpha2.DataSourceTypeObjectRef
+	objRef := vd.Spec.DataSource.ObjectRef
+	if isObjectRef && objRef != nil && objRef.Kind == v1alpha2.VirtualDiskObjectRefKindVirtualImage {
+		vi, err := object.FetchObject(ctx, types.NamespacedName{Namespace: vd.Namespace, Name: objRef.Name}, v.client, &v1alpha2.VirtualImage{})
+		if err != nil {
+			return err
+		}
+
+		if vi == nil || vi.Status.Phase != v1alpha2.ImageReady || vi.Spec.Storage == v1alpha2.StorageContainerRegistry {
+			return nil
+		}
+
+		vdSc, err := v.extractVDStorageClassName(ctx, vd)
+		if err != nil {
+			return err
+		}
+
+		if vdSc != vi.Status.StorageClassName {
+			return fmt.Errorf("virtual disk storage class %q does not match virtual image storage class %q", vdSc, vi.Status.StorageClassName)
+		}
+	}
+
+	return nil
+}
+
+func (v *VirtualImagePVCStorageClassValidator) extractVDStorageClassName(ctx context.Context, vd *v1alpha2.VirtualDisk) (string, error) {
+	if vd.Status.StorageClassName != "" {
+		return vd.Status.StorageClassName, nil
+	}
+
+	if vd.Spec.PersistentVolumeClaim.StorageClass != nil {
+		return *vd.Spec.PersistentVolumeClaim.StorageClass, nil
+	}
+
+	moduleStorageClass, err := v.scService.GetModuleStorageClass(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	if moduleStorageClass != nil {
+		return moduleStorageClass.Name, nil
+	}
+
+	defaultStorageClass, err := v.scService.GetDefaultStorageClass(ctx)
+	if err != nil && !errors.Is(err, service.ErrDefaultStorageClassNotFound) {
+		return "", err
+	}
+
+	if defaultStorageClass != nil {
+		return defaultStorageClass.Name, nil
+	}
+
+	return "", fmt.Errorf("storage class for VirtualDisk %q cannot be determined", vd.Name)
+}
