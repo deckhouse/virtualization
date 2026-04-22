66b9afb7d43a33264f12aff28b903e252406bbe1
Daniil Antoshin <daniil.antoshin@flant.com>
2026-04-22 11:05:58 +0200

    test(cvi): rewrite source test to ginkgo
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- images/virtualization-artifact/pkg/controller/cvi/internal/source/object_ref_vi_on_pvc_test.go
+++ images/virtualization-artifact/pkg/controller/cvi/internal/source/object_ref_vi_on_pvc_test.go
@@ -18,9 +18,10 @@ package source
 
 import (
 	"context"
-	"testing"
 	"time"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -33,82 +34,71 @@ import (
 	"github.com/deckhouse/virtualization/api/core/v1alpha2/cvicondition"
 )
 
-func TestObjectRefVirtualImageOnPvcSyncCopiesSizeFromVirtualImage(t *testing.T) {
-	pod := &corev1.Pod{
-		Status: corev1.PodStatus{
-			Phase: corev1.PodSucceeded,
-		},
-	}
-
-	importer := &ImporterMock{
-		GetPodFunc: func(_ context.Context, _ supplements.Generator) (*corev1.Pod, error) {
-			return pod, nil
-		},
-	}
-	stat := &StatMock{
-		CheckPodFunc: func(_ *corev1.Pod) error {
-			return nil
-		},
-		GetDVCRImageNameFunc: func(_ *corev1.Pod) string {
-			return "registry.example.com/image:test"
-		},
-	}
-	recorder := &eventrecord.EventRecorderLoggerMock{
-		EventFunc: func(_ client.Object, _, _, _ string) {},
-	}
-
-	syncer := NewObjectRefVirtualImageOnPvc(recorder, importer, &dvcr.Settings{}, stat)
-
-	vi := &v1alpha2.VirtualImage{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "vi",
-			Namespace: "test-ns",
-		},
-		Status: v1alpha2.VirtualImageStatus{
-			Size: v1alpha2.ImageStatusSize{
-				Stored:        "12Gi",
-				StoredBytes:   "12884901888",
-				Unpacked:      "10Gi",
-				UnpackedBytes: "10737418240",
+var _ = Describe("ObjectRef VirtualImage on PVC", func() {
+	It("copies size from VirtualImage when provisioning pod is completed", func() {
+		pod := &corev1.Pod{
+			Status: corev1.PodStatus{
+				Phase: corev1.PodSucceeded,
 			},
-			CDROM:  true,
-			Format: "raw",
-			Target: v1alpha2.VirtualImageStatusTarget{
-				PersistentVolumeClaim: "vi-pvc",
+		}
+
+		importer := &ImporterMock{
+			GetPodFunc: func(_ context.Context, _ supplements.Generator) (*corev1.Pod, error) {
+				return pod, nil
 			},
-		},
-	}
+		}
+		stat := &StatMock{
+			CheckPodFunc: func(_ *corev1.Pod) error {
+				return nil
+			},
+			GetDVCRImageNameFunc: func(_ *corev1.Pod) string {
+				return "registry.example.com/image:test"
+			},
+		}
+		recorder := &eventrecord.EventRecorderLoggerMock{
+			EventFunc: func(_ client.Object, _, _, _ string) {},
+		}
 
-	cvi := &v1alpha2.ClusterVirtualImage{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:       "cvi",
-			Generation: 1,
-			UID:        "11111111-1111-1111-1111-111111111111",
-		},
-	}
+		syncer := NewObjectRefVirtualImageOnPvc(recorder, importer, &dvcr.Settings{}, stat)
 
-	cb := conditions.NewConditionBuilder(cvicondition.ReadyType).Generation(cvi.Generation)
+		vi := &v1alpha2.VirtualImage{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "vi",
+				Namespace: "test-ns",
+			},
+			Status: v1alpha2.VirtualImageStatus{
+				Size: v1alpha2.ImageStatusSize{
+					Stored:        "12Gi",
+					StoredBytes:   "12884901888",
+					Unpacked:      "10Gi",
+					UnpackedBytes: "10737418240",
+				},
+				CDROM:  true,
+				Format: "raw",
+				Target: v1alpha2.VirtualImageStatusTarget{
+					PersistentVolumeClaim: "vi-pvc",
+				},
+			},
+		}
 
-	res, err := syncer.Sync(context.Background(), cvi, vi, cb)
-	if err != nil {
-		t.Fatalf("Sync returned error: %v", err)
-	}
-	if res.RequeueAfter != time.Second {
-		t.Fatalf("expected requeue after 1s, got %+v", res)
-	}
-	if cvi.Status.Phase != v1alpha2.ImageReady {
-		t.Fatalf("expected phase %q, got %q", v1alpha2.ImageReady, cvi.Status.Phase)
-	}
-	if cvi.Status.Size != vi.Status.Size {
-		t.Fatalf("expected size %+v, got %+v", vi.Status.Size, cvi.Status.Size)
-	}
-	if cvi.Status.CDROM != vi.Status.CDROM {
-		t.Fatalf("expected CDROM %t, got %t", vi.Status.CDROM, cvi.Status.CDROM)
-	}
-	if cvi.Status.Format != vi.Status.Format {
-		t.Fatalf("expected format %q, got %q", vi.Status.Format, cvi.Status.Format)
-	}
-	if cvi.Status.Target.RegistryURL != "registry.example.com/image:test" {
-		t.Fatalf("unexpected registry URL %q", cvi.Status.Target.RegistryURL)
-	}
-}
+		cvi := &v1alpha2.ClusterVirtualImage{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:       "cvi",
+				Generation: 1,
+				UID:        "11111111-1111-1111-1111-111111111111",
+			},
+		}
+
+		cb := conditions.NewConditionBuilder(cvicondition.ReadyType).Generation(cvi.Generation)
+
+		res, err := syncer.Sync(context.Background(), cvi, vi, cb)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.RequeueAfter).To(Equal(time.Second))
+		Expect(cvi.Status.Phase).To(Equal(v1alpha2.ImageReady))
+		Expect(cvi.Status.Size).To(Equal(vi.Status.Size))
+		Expect(cvi.Status.CDROM).To(Equal(vi.Status.CDROM))
+		Expect(cvi.Status.Format).To(Equal(vi.Status.Format))
+		Expect(cvi.Status.Target.RegistryURL).To(Equal("registry.example.com/image:test"))
+	})
+})
