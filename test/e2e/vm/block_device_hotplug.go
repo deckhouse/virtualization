2b637e77adbcea8d9f15275e3c3351f6e9e31d16
Daniil Loktev <lokt.daniil@gmail.com>
2026-05-07 19:14:40 +0300

    add label for e2e test
    Signed-off-by: Daniil Loktev <lokt.daniil@gmail.com>

--- test/e2e/vm/block_device_hotplug.go
+++ test/e2e/vm/block_device_hotplug.go
@@ -34,10 +34,11 @@ import (
 	"github.com/deckhouse/virtualization/api/core/v1alpha2"
 	"github.com/deckhouse/virtualization/test/e2e/internal/framework"
 	"github.com/deckhouse/virtualization/test/e2e/internal/object"
+	"github.com/deckhouse/virtualization/test/e2e/internal/precheck"
 	"github.com/deckhouse/virtualization/test/e2e/internal/util"
 )
 
-var _ = Describe("VirtualMachineBlockDeviceHotplug", Ordered, func() {
+var _ = Describe("VirtualMachineBlockDeviceHotplug", Ordered, Label(precheck.NoPrecheck), func() {
 	f := framework.NewFramework("vm-bd-hotplug")
 
 	var (
