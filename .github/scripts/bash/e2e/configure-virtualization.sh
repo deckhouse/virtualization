e741f8b4c9c000a889b546138f5bdc38c8b36e75
Maksim Fedotov <maksim.fedotov@flant.com>
2026-06-29 19:06:52 +0300

    DO NOT MERGE: disable featuregates for nested release pipeline
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- .github/scripts/bash/e2e/configure-virtualization.sh
+++ .github/scripts/bash/e2e/configure-virtualization.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Copyright 2026 Flant JSC
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -168,8 +168,6 @@ spec:
       - 192.168.10.0/24
     featureGates:
       - HotplugCPUWithLiveMigration
-      - HotplugMemoryWithLiveMigration
-      - HotplugCPUAndMemoryWithInPlaceResize
   source: deckhouse-dev
   version: 1
 ---
