c9b8013b43c428d7a2b799a369169fb33e9f55a0
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2026-07-20 17:55:45 +0300

    [chore] fix license
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- images/pvc-artifact/pkg/image/filefmt.go
+++ images/pvc-artifact/pkg/image/filefmt.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2018 The CDI Authors.
+Copyright 2026 Flant JSC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
